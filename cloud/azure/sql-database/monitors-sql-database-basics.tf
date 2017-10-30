3774358e12b983a1c74643c54fba1888620438ce
Kevin Pecquet <kpecquet@morea.fr>
2017-10-30 15:48:26 +0100

    MON-75 SQL DB monitors init

---
+++ cloud/azure/sql-database/monitors-sql-database-basics.tf
@@ -0,0 +1,109 @@
+data "template_file" "filter" {
+  template = "$${filter}"
+
+  vars {
+    filter = "${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_sqldb:enabled,env:%s",var.environment) : "*"}"
+  }
+}
+
+resource "datadog_monitor" "sql-database_cpu_90_15min" {
+  name    = "[${var.environment}] SQL Database CPU high > 90% for 15 min on {{name}}"
+  message = "${message}"
+
+  count = "${var.dd_azure_sqldb == "enabled" ? 1 : 0 }"
+
+  query = "avg(last_15m):avg:azure.sql_servers_databases.cpu_percent{${data.template_file.filter.rendered}} by {name,resource_group} > ${var.cpu_threshold_critical}"
+  type  = "query alert"
+
+  thresholds {
+    critical = "${var.cpu_threshold_critical}"
+  }
+
+  notify_no_data      = true
+  evaluation_delay    = "${var.delay}"
+  renotify_interval   = 0
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+  new_host_delay      = "${var.delay}"
+  no_data_timeframe   = 20
+}
+
+resource "datadog_monitor" "sql-database_free_space_low" {
+  name    = "[${var.environment}] SQL Database free space < 10 % on {{name}}"
+  message = "${message}"
+
+  type  = "query alert"
+  query = "avg(last_15m):avg:azure.sql_servers_databases.storage_percent{${data.template_file.filter.rendered}} by {name,resource_group} > 90"
+
+  count = "${var.dd_azure_sqldb == "enabled" ? 1 : 0 }"
+
+  thresholds {
+    warning  = "${var.diskspace_threshold_warning}"
+    critical = "${var.diskspace_threshold_critical}"
+  }
+
+  notify_no_data      = true
+  evaluation_delay    = "${var.delay}"
+  renotify_interval   = 0
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+  new_host_delay      = "${var.delay}"
+  no_data_timeframe   = 20
+}
+
+resource "datadog_monitor" "sql-database_dtu_consumption_high" {
+  name    = "[${var.environment}] DTU Consumption on {{name}} > 90"
+  message = "${message}"
+
+  type  = "query alert"
+  query = "avg(last_15m):azure.sql_servers_databases.dtu_consumption_percent{${data.template_file.filter.rendered}} by {name,resource_group} > 90"
+
+  count = "${var.dd_azure_sqldb == "enabled" ? 1 : 0 }"
+
+  thresholds {
+    warning  = "${var.dtu_threshold_warning}"
+    critical = "${var.dtu_threshold_critical}"
+  }
+
+  notify_no_data      = true
+  evaluation_delay    = "${var.delay}"
+  renotify_interval   = 0
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+  new_host_delay      = "${var.delay}"
+  no_data_timeframe   = 20
+}
+
+resource "datadog_monitor" "sql-database_deadlocks_count" {
+  name    = "[${var.environment}] SQL Deadlocks too high on {{name}}"
+  message = "${message}"
+
+  type  = "query alert"
+  query = "sum(last_5m):avg:azure.sql_servers_databases.deadlock{${data.template_file.filter.rendered}} by {name,resource_group}.as_count() > ${var.deadlock_threshold_critical}"
+
+  count = "${var.dd_azure_sqldb == "enabled" ? 1 : 0 }"
+
+  thresholds {
+    critical = "${var.deadlock_threshold_critical}"
+  }
+
+  notify_no_data      = false
+  evaluation_delay    = "${var.delay}"
+  renotify_interval   = 0
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+  new_host_delay      = "${var.delay}"
+  no_data_timeframe   = 20
+}
