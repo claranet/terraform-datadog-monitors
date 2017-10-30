2444faee84451946bcd6c5edc9900ef304becfd6
Jérôme Respaut <jerome.respaut@fr.clara.net>
2017-10-30 11:34:42 +0100

    MON-76: Azure Redis - DataDog Monitors

---
+++ cloud/azure/redis/monitors-azure-redis.tf
@@ -0,0 +1,42 @@
+resource "datadog_monitor" "status" {
+  name    = "[${var.environment}] Redis {{name}} is down"
+  message = "{{#is_alert}}\n${var.critical_escalation_group} \n{{/is_alert}} \n{{#is_recovery}}\n${var.critical_escalation_group}\n{{/is_recovery}}"
+
+  query = "avg(last_5m):avg:azure.cache_redis.status{*} by {name,resource_group} != 1"
+  type  = "query alert"
+
+  notify_no_data      = false
+  evaluation_delay    = "${var.delay}"
+  renotify_interval   = 60
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+  new_host_delay      = "${var.delay}"
+  no_data_timeframe   = 20
+}
+
+resource "datadog_monitor" "evictedkeys" {
+  name    = "[${var.environment}] Redis {{value}} evictedkeys on {{name}}"
+  message = "{{#is_alert}}\n${var.critical_escalation_group} \n{{/is_alert}} \n{{#is_recovery}}\n${var.critical_escalation_group}\n{{/is_recovery}}"
+
+  query = "avg(last_5m):avg:azure.cache_redis.evictedkeys{*} by {name,resource_group} > ${var.evictedkeys_threshold_critical}"
+  type  = "query alert"
+
+  thresholds {
+    warning  = "${var.evictedkeys_threshold_warning}"
+    critical = "${var.evictedkeys_threshold_critical}"
+  }
+
+  notify_no_data      = false
+  evaluation_delay    = "${var.delay}"
+  renotify_interval   = 60
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+  new_host_delay      = "${var.delay}"
+  no_data_timeframe   = 20
+}
