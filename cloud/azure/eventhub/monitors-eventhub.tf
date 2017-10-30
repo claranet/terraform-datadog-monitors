b35794fbb4661682d530d5393d9bad842cc2e2f8
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2017-10-30 11:40:16 +0100

    MON-77 Azure Event Hub monitors

---
+++ cloud/azure/eventhub/monitors-eventhub.tf
@@ -0,0 +1,86 @@
+resource "datadog_monitor" "eventhub_status" {
+  name    = "[${var.environment}] Event Hub status"
+  message = "${var.down_message}"
+
+  query = <<EOF
+      avg(last_5m): avg:azure.eventhub_namespaces.status{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region} != 1
+      EOF
+  type  = "query alert"
+
+  notify_no_data      = true
+  evaluation_delay    = "${var.delay}"
+  renotify_interval   = 60
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+  new_host_delay      = "${var.delay}"
+  no_data_timeframe   = 20
+}
+
+resource "datadog_monitor" "eventhub_failed_requests" {
+  name    = "[${var.environment}] Event Hub failed requests"
+  message = "${var.failed_requests_message}"
+
+  query = <<EOF
+        avg(last_5m): (
+          avg:azure.eventhub_namespaces.failed_requests{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region}
+        ) * 100 / (
+          avg:azure.eventhub_namespaces.successful_requests{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region} +
+          avg:azure.eventhub_namespaces.failed_requests{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region}
+        ) > ${var.failed_requests_rate_thresold_critical}
+        EOF
+  type  = "query alert"
+
+  thresholds {
+    critical = "${var.failed_requests_rate_thresold_critical}"
+    warning  = "${var.failed_requests_rate_thresold_warning}"
+  }
+
+  notify_no_data      = false
+  evaluation_delay    = "${var.delay}"
+  renotify_interval   = 60
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+  new_host_delay      = "${var.delay}"
+  no_data_timeframe   = 20
+}
+
+resource "datadog_monitor" "eventhub_errors" {
+  name    = "[${var.environment}] Event Hub errors"
+  message = "${var.errors_message}"
+
+  query = <<EOF
+        avg(last_5m): (
+          avg:azure.eventhub_namespaces.internal_server_errors{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region} +
+          avg:azure.eventhub_namespaces.server_busy_errors{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region} +
+          avg:azure.eventhub_namespaces.other_errors{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region}
+        ) * 100 / (
+          avg:azure.eventhub_namespaces.successful_requests{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region} +
+          avg:azure.eventhub_namespaces.internal_server_errors{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region} +
+          avg:azure.eventhub_namespaces.server_busy_errors{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region} +
+          avg:azure.eventhub_namespaces.other_errors{${var.use_filter_tags == "true" ? format("dd_monitoring:enabled,dd_azure_eventhub:enabled,env:%s", var.environment) : "*"}} by {name,resource_group,region}
+        ) > ${var.errors_rate_thresold_critical}
+        EOF
+  type  = "query alert"
+
+  thresholds {
+    critical = "${var.errors_rate_thresold_critical}"
+    warning  = "${var.errors_rate_thresold_warning}"
+  }
+
+  notify_no_data      = false
+  evaluation_delay    = "${var.delay}"
+  renotify_interval   = 60
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+  new_host_delay      = "${var.delay}"
+  no_data_timeframe   = 20
+}
