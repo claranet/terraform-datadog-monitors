193352c212277fac91d51ad336b704f7cde8d54c
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2017-10-30 18:09:03 +0100

    MON-80 Add IOT Hub in Names

--- cloud/azure/iothubs/monitors-iothubs.tf
+++ cloud/azure/iothubs/monitors-iothubs.tf
@@ -1,5 +1,5 @@
 resource "datadog_monitor" "too_many_jobs_failed" {
-  name    = "[${var.environment}] Too many jobs failed on {{name}} "
+  name    = "[${var.environment}] IOT Hub Too many jobs failed on {{name}}"
   message = "${var.jobs_failed_message}"
 
   query = "sum(last_5m):( avg:azure.devices_iothubs.jobs.failed{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() / ( avg:azure.devices_iothubs.jobs.failed{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() + avg:azure.devices_iothubs.jobs.completed{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() ) ) * 100 > ${var.jobs_failed_threshold_critical}"
@@ -23,7 +23,7 @@ resource "datadog_monitor" "too_many_jobs_failed" {
 }
 
 resource "datadog_monitor" "too_many_list_jobs_failed" {
-  name    = "[${var.environment}] Too many list_jobs failure on {{name}} "
+  name    = "[${var.environment}] IOT Hub Too many list_jobs failure on {{name}}"
   message = "${var.listjobs_failed_message}"
 
   query = "sum(last_5m):( avg:azure.devices_iothubs.jobs.list_jobs.failure{subscription_id:${var.subscription_id}} by {resource_group,name}.as_count() / ( avg:azure.devices_iothubs.jobs.list_jobs.success{subscription_id:${var.subscription_id}} by {resource_group,name}.as_count() + avg:azure.devices_iothubs.jobs.list_jobs.failure{subscription_id:${var.subscription_id}} by {resource_group,name}.as_count() ) ) * 100 > ${var.listjobs_failed_threshold_critical}"
@@ -47,7 +47,7 @@ resource "datadog_monitor" "too_many_list_jobs_failed" {
 }
 
 resource "datadog_monitor" "too_many_query_jobs_failed" {
-  name    = "[${var.environment}] Too many query_jobs failed on {{name}} "
+  name    = "[${var.environment}] IOT Hub Too many query_jobs failed on {{name}}"
   message = "${var.queryjobs_failed_message}"
 
   query = "sum(last_5m):( avg:azure.devices_iothubs.jobs.query_jobs.failure{subscription_id:${var.subscription_id}} by {resource_group,name}.as_count() / ( avg:azure.devices_iothubs.jobs.query_jobs.success{subscription_id:${var.subscription_id}} by {resource_group,name}.as_count() + avg:azure.devices_iothubs.jobs.query_jobs.failure{subscription_id:${var.subscription_id}} by {resource_group,name}.as_count() ) ) * 100 > ${var.queryjobs_failed_threshold_critical}"
@@ -55,7 +55,7 @@ resource "datadog_monitor" "too_many_query_jobs_failed" {
 
   thresholds {
     warning  = "${var.queryjobs_failed_threshold_warning}"
-    critical = "${var.queryjobs_failed_threshold_warning}"
+    critical = "${var.queryjobs_failed_threshold_critical}"
   }
 
   notify_no_data      = false
@@ -71,7 +71,7 @@ resource "datadog_monitor" "too_many_query_jobs_failed" {
 }
 
 resource "datadog_monitor" "status" {
-  name    = "[${var.environment}] Status is not ok on {{name}} "
+  name    = "[${var.environment}] IOT Hub Status is not ok on {{name}}"
   message = "${var.status_message}"
 
   query = "avg(last_5m):avg:azure.devices_iothubs.status{subscription_id:${var.subscription_id}} by {name,resource_group} < 1"
@@ -90,7 +90,7 @@ resource "datadog_monitor" "status" {
 }
 
 resource "datadog_monitor" "total_devices" {
-  name    = "[${var.environment}] Total devices is wrong on {{name}} "
+  name    = "[${var.environment}] IOT Hub Total devices is wrong on {{name}}"
   message = "${var.total_devices_message}"
 
   query = "avg(last_5m):avg:azure.devices_iothubs.devices.total_devices{subscription_id:${var.subscription_id}} by {name,resource_group} == 0"
@@ -109,7 +109,7 @@ resource "datadog_monitor" "total_devices" {
 }
 
 resource "datadog_monitor" "too_many_c2d_methods_failed" {
-  name    = "[${var.environment}] Too many c2d methods failure on {{name}} "
+  name    = "[${var.environment}] IOT Hub Too many c2d methods failure on {{name}}"
   message = "${var.c2d_methods_failed_message}"
 
   query = "avg(last_5m):( avg:azure.devices_iothubs.c2d.methods.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() / ( avg:azure.devices_iothubs.c2d.methods.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() + avg:azure.devices_iothubs.c2d.methods.success{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() ) ) * 100 > ${var.c2d_methods_failed_threshold_critical}"
@@ -133,7 +133,7 @@ resource "datadog_monitor" "too_many_c2d_methods_failed" {
 }
 
 resource "datadog_monitor" "too_many_c2d_twin_read_failed" {
-  name    = "[${var.environment}] Too many c2d twin read failure on {{name}} "
+  name    = "[${var.environment}] IOT Hub Too many c2d twin read failure on {{name}}"
   message = "${var.c2d_twin_read_failed_message}"
 
   query = "avg(last_5m):( avg:azure.devices_iothubs.c2d.twin.read.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() / ( avg:azure.devices_iothubs.c2d.twin.read.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() + avg:azure.devices_iothubs.c2d.twin.read.success{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() ) ) * 100 > ${var.c2d_twin_read_failed_threshold_critical}"
@@ -157,7 +157,7 @@ resource "datadog_monitor" "too_many_c2d_twin_read_failed" {
 }
 
 resource "datadog_monitor" "too_many_c2d_twin_update_failed" {
-  name    = "[${var.environment}] Too many c2d twin update failure on {{name}} "
+  name    = "[${var.environment}] IOT Hub Too many c2d twin update failure on {{name}}"
   message = "${var.c2d_twin_update_failed_message}"
 
   query = "avg(last_5m):( avg:azure.devices_iothubs.c2d.twin.update.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() / ( avg:azure.devices_iothubs.c2d.twin.update.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() + avg:azure.devices_iothubs.c2d.twin.update.success{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() ) ) * 100 > ${var.c2d_twin_update_failed_threshold_critical}"
@@ -181,7 +181,7 @@ resource "datadog_monitor" "too_many_c2d_twin_update_failed" {
 }
 
 resource "datadog_monitor" "too_many_d2c_twin_read_failed" {
-  name    = "[${var.environment}] Too many d2c twin read failure on {{name}} "
+  name    = "[${var.environment}] IOT Hub Too many d2c twin read failure on {{name}}"
   message = "${var.d2c_twin_read_failed_message}"
 
   query = "avg(last_5m):( avg:azure.devices_iothubs.d2c.twin.read.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() / ( avg:azure.devices_iothubs.d2c.twin.read.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() + avg:azure.devices_iothubs.d2c.twin.read.success{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() ) ) * 100 > ${var.d2c_twin_read_failed_threshold_critical}"
@@ -205,7 +205,7 @@ resource "datadog_monitor" "too_many_d2c_twin_read_failed" {
 }
 
 resource "datadog_monitor" "too_many_d2c_twin_update_failed" {
-  name    = "[${var.environment}] Too many d2c twin update failure on {{name}} "
+  name    = "[${var.environment}] IOT Hub Too many d2c twin update failure on {{name}}"
   message = "${var.d2c_twin_update_failed_message}"
 
   query = "avg(last_5m):( avg:azure.devices_iothubs.d2c.twin.update.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() / ( avg:azure.devices_iothubs.d2c.twin.update.failure{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() + avg:azure.devices_iothubs.d2c.twin.update.success{subscription_id:${var.subscription_id}} by {name,resource_group}.as_count() ) ) * 100 > ${var.d2c_twin_update_failed_threshold_critical}"
