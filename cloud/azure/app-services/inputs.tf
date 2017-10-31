81df985f3297bcf3e993fef42f3a98146339bce0
Marc-Antoine ADELISE <maadelise@morea.fr>
2017-10-31 10:08:19 +0100

    MON-74: Response time, memory usage, http 404 status code and non 2xx http response status code percentage monitoring.

--- cloud/azure/app-services/inputs.tf
+++ cloud/azure/app-services/inputs.tf
@@ -1,5 +1,11 @@
-variable "filter_tags" {
+variable "environment" {}
+
+variable "use_filter_tags" {
   default = "*"
+}
+
+variable "critical_escalation_group" {
+  default = "HO_Dummy"
 }
 
 ###################################
@@ -9,7 +15,7 @@ variable "response_time_appserv_eval_delay" {
   default = 600
 }
 
-variable "response_time_critical_threshold" {
+variable "response_time_threshold_critical" {
   default = 0.8
   description = "Alerting threshold in seconds"
 }
@@ -22,6 +28,11 @@ variable "response_time_threshold_warning" {
 variable "response_time_last_time_window_code" {
   default = "1h"
   description = "Query time window code, can be: 1h|4h|1d|2d|1w|1m|3m... to write last_#m (1, 5, 10, 15, or 30), last_#h (1, 2, or 4), or last_#d (1 or 2)"
+}
+
+variable "response_time_require_full_window" {
+  default = false
+  description = "A boolean indicating whether this monitor needs a full window of data before it's evaluated. We highly recommend you set this to False for sparse metrics, otherwise some evaluations will be skipped. Default: True for 'on average', 'at all times' and 'in total' aggregation. False otherwise."
 }
 
 variable "response_time_tags" {
@@ -39,10 +50,10 @@ variable "response_time_include_tags" {
   description = "A boolean indicating whether notifications from this monitor will automatically insert its triggering tags into the title. Defaults to true."
 }
 
-variable "response_time_notify_no_data" {
-  default = true
-  description = " boolean indicating whether this monitor will notify when data stops reporting. Defaults to true."
-}
+# variable "response_time_notify_no_data" {
+#   default = true
+#   description = " boolean indicating whether this monitor will notify when data stops reporting. Defaults to true."
+# }
 
 variable "response_time_renotify_interval" {
   default = 0
@@ -76,6 +87,11 @@ variable "memory_usage_last_time_window_code" {
   description = "Query time window code, can be: 1h|4h|1d|2d|1w|1m|3m... to write last_#m (1, 5, 10, 15, or 30), last_#h (1, 2, or 4), or last_#d (1 or 2)"
 }
 
+variable "memory_usage_require_full_window" {
+  default = false
+  description = "A boolean indicating whether this monitor needs a full window of data before it's evaluated. We highly recommend you set this to False for sparse metrics, otherwise some evaluations will be skipped. Default: True for 'on average', 'at all times' and 'in total' aggregation. False otherwise."
+}
+
 variable "memory_usage_tags" {
   default = []
   description = "A list of tags to associate with your monitor. This can help you categorize and filter monitors in the manage monitors page of the UI. Note: it's not currently possible to filter by these tags when querying via the API"
@@ -91,10 +107,10 @@ variable "memory_usage_include_tags" {
   description = "A boolean indicating whether notifications from this monitor will automatically insert its triggering tags into the title. Defaults to true."
 }
 
-variable "memory_usage_notify_no_data" {
-  default = true
-  description = " boolean indicating whether this monitor will notify when data stops reporting. Defaults to true."
-}
+# variable "memory_usage_notify_no_data" {
+#   default = true
+#   description = " boolean indicating whether this monitor will notify when data stops reporting. Defaults to true."
+# }
 
 variable "memory_usage_renotify_interval" {
   default = 0
@@ -102,6 +118,128 @@ variable "memory_usage_renotify_interval" {
 }
 
 variable "memory_usage_escalation_message" {
+  default = "Escalation message @pagerduty"
+  description = "A message to include with a re-notification. Supports the '@username' notification allowed elsewhere."
+}
+
+#################################
+###   HTTP 404 status pages   ###
+#################################
+variable "http_404_errors_count_rate_limit" {
+  default = 30
+}
+
+variable "http_404_errors_count_rate_appserv_eval_delay" {
+  default = 600
+}
+
+variable "http_404_errors_count_rate_threshold_critical" {
+  default = 30
+  description = "Alerting threshold (number of requests)"
+}
+
+variable "http_404_errors_count_rate_threshold_warning" {
+  default = 10
+  description = "Warning threshold (number of requests)"
+}
+
+variable "http_404_errors_count_rate_last_time_window_code" {
+  default = "5m"
+  description = "Query time window code, can be: 1h|4h|1d|2d|1w|1m|3m... to write last_#m (1, 5, 10, 15, or 30), last_#h (1, 2, or 4), or last_#d (1 or 2)"
+}
+
+variable "http_404_errors_count_rate_require_full_window" {
+  default = true
+  description = "A boolean indicating whether this monitor needs a full window of data before it's evaluated. We highly recommend you set this to False for sparse metrics, otherwise some evaluations will be skipped. Default: True for 'on average', 'at all times' and 'in total' aggregation. False otherwise."
+}
+
+variable "http_404_errors_count_rate_tags" {
+  default = []
+  description = "A list of tags to associate with your monitor. This can help you categorize and filter monitors in the manage monitors page of the UI. Note: it's not currently possible to filter by these tags when querying via the API"
+}
+
+variable "http_404_errors_count_rate_timeout_h" {
+  default = false
+  description = "The number of hours of the monitor not reporting data before it will automatically resolve from a triggered state. Defaults to false."
+}
+
+variable "http_404_errors_count_rate_include_tags" {
+  default = false
+  description = "A boolean indicating whether notifications from this monitor will automatically insert its triggering tags into the title. Defaults to true."
+}
+
+# variable "http_404_errors_count_rate_notify_no_data" {
+#   default = true
+#   description = " boolean indicating whether this monitor will notify when data stops reporting. Defaults to true."
+# }
+
+variable "http_404_errors_count_rate_renotify_interval" {
+  default = 0
+  description = "The number of minutes after the last notification before a monitor will re-notify on the current status. It will only re-notify if it's not resolved."
+}
+
+variable "http_404_errors_count_rate_escalation_message" {
+  default = "Escalation message @pagerduty"
+  description = "A message to include with a re-notification. Supports the '@username' notification allowed elsewhere."
+}
+
+#################################
+###   HTTP 202 status pages   ###
+#################################
+variable "http_2xx_status_rate_limit" {
+  default = 30
+}
+
+variable "http_2xx_status_rate_appserv_eval_delay" {
+  default = 600
+}
+
+variable "http_2xx_status_rate_threshold_critical" {
+  default = 0.9
+  description = "Alerting threshold (percentage)"
+}
+
+variable "http_2xx_status_rate_threshold_warning" {
+  default = 0.95
+  description = "Warning threshold (percentage)"
+}
+
+variable "http_2xx_status_rate_last_time_window_code" {
+  default = "5m"
+  description = "Query time window code, can be: 1h|4h|1d|2d|1w|1m|3m... to write last_#m (1, 5, 10, 15, or 30), last_#h (1, 2, or 4), or last_#d (1 or 2)"
+}
+
+variable "http_2xx_status_rate_require_full_window" {
+  default = true
+  description = "A boolean indicating whether this monitor needs a full window of data before it's evaluated. We highly recommend you set this to False for sparse metrics, otherwise some evaluations will be skipped. Default: True for 'on average', 'at all times' and 'in total' aggregation. False otherwise."
+}
+
+variable "http_2xx_status_rate_tags" {
+  default = []
+  description = "A list of tags to associate with your monitor. This can help you categorize and filter monitors in the manage monitors page of the UI. Note: it's not currently possible to filter by these tags when querying via the API"
+}
+
+variable "http_2xx_status_rate_timeout_h" {
+  default = false
+  description = "The number of hours of the monitor not reporting data before it will automatically resolve from a triggered state. Defaults to false."
+}
+
+variable "http_2xx_status_rate_include_tags" {
+  default = false
+  description = "A boolean indicating whether notifications from this monitor will automatically insert its triggering tags into the title. Defaults to true."
+}
+
+# variable "http_2xx_status_rate_notify_no_data" {
+#   default = true
+#   description = " boolean indicating whether this monitor will notify when data stops reporting. Defaults to true."
+# }
+
+variable "http_2xx_status_rate_renotify_interval" {
+  default = 0
+  description = "The number of minutes after the last notification before a monitor will re-notify on the current status. It will only re-notify if it's not resolved."
+}
+
+variable "http_2xx_status_rate_escalation_message" {
   default = "Escalation message @pagerduty"
   description = "A message to include with a re-notification. Supports the '@username' notification allowed elsewhere."
 }
