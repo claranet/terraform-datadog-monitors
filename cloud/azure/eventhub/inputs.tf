9e55ce3772db1e029cb51c7aedee2a4bce6042e5
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2017-11-03 20:41:57 +0100

    MON-77 add tags and subscription_id

--- cloud/azure/eventhub/inputs.tf
+++ cloud/azure/eventhub/inputs.tf
@@ -1,5 +1,27 @@
-variable "environment" {}
+# Global Terraform
+variable "environment" {
+  description = "Architecture environment"
+  type = "string"
+}
 
+variable "subscription_id" {
+  description = "Azure account id used as filter for monitors"
+  type = "string"
+}
+
+variable "provider" {
+  description = "Cloud provider which the monitor and its based metric depend on"
+  type = "string"
+  default = "azure"
+}
+
+variable "service" {
+  description = "Service monitored by this set of monitors"
+  type = "string"
+  default = "storage"
+}
+
+# Global DataDog
 variable "message" {
   description = "Message sent when an alert is triggered"
 }
@@ -7,6 +29,11 @@ variable "message" {
 variable "delay" {
   description = "Delay in seconds for the metric evaluation"
   default = 600
+}
+
+variable "use_filter_tags" {
+  description = "Filter the data with service tags if true"
+  default     = "true"
 }
 
 variable "failed_requests_rate_thresold_critical" {
@@ -27,9 +54,4 @@ variable "errors_rate_thresold_critical" {
 variable "errors_rate_thresold_warning" {
   description = "Errors ratio (percentage) to trigger a warning alert"
   default = 1
-}
-
-variable "use_filter_tags" {
-  description = "Filter the data with service tags if true"
-  default = "true"
 }
