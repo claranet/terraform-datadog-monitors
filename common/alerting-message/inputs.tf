5dc7a7518e6b4332a082b3b34d7c88d912eb8847
Jérôme Respaut <jerome.respaut@fr.clara.net>
2017-11-06 10:20:10 +0100

    MON-82: Add some variable to be able to surround text message

--- common/alerting-message/inputs.tf
+++ common/alerting-message/inputs.tf
@@ -5,3 +5,13 @@ variable "oncall_24x7" {
 variable "oncall_office_hours" {
   type = "string"
 }
+
+variable "prepend_text" {
+  type    = "string"
+  default = ""
+}
+
+variable "append_text" {
+  type    = "string"
+  default = ""
+}
--- common/alerting-message/main.tf
+++ common/alerting-message/main.tf
@@ -1,9 +1,11 @@
 data "template_file" "alerting-message" {
   template = <<EOF
+$${prepend_text}
 {{#is_alert}}$${oncall_24x7}{{/is_alert}}
 {{#is_recovery}}$${oncall_24x7}{{/is_recovery}}
 {{#is_warning}}$${oncall_office_hours}{{/is_warning}}
 {{#is_warning_recovery}}$${oncall_office_hours}{{/is_warning_recovery}}
+$${append_text}
 EOF
 
   vars {
