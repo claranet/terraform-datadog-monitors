45071d6c303b521243008f673388ea20d5be166b
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2018-02-19 15:37:11 +0100

    MON-91 - Update PR to follow recommandations

--- cloud/aws/vpn/inputs.tf
+++ cloud/aws/vpn/inputs.tf
@@ -1,1 +1,19 @@
-../../../inputs.tf
+variable env {}
+
+variable "vpn_tunnel_address" {
+  description = "List the VPN tunnels you want to monitor"
+  type        = "list"
+  default     = []
+}
+
+variable "vpn_config" {
+  description = "Ok means that both tunnels are up, warning means only one tunnel is up and critical means that both tunnels are down "
+  type        = "map"
+
+  default = {
+    delay    = 900
+    warning  = 1
+    critical = 0
+    ok       = 2
+  }
+}
--- inputs.tf
+++ inputs.tf
@@ -109,25 +109,6 @@ variable "elb_backend_latency" {
   }
 }
 
-## VPN
-variable "vpn_tunnel_address" {
-  description = "List the VPN tunnels you want to monitor"
-  type        = "list"
-  default     = []
-}
-
-variable "vpn_config" {
-  description = "Ok means that both tunnels are up, warning means only one tunnel is up and critical means that both tunnels are down "
-  type        = "map"
-
-  default = {
-    delay    = 900
-    warning  = 1
-    critical = 0
-    ok       = 2
-  }
-}
-
 ##apache nginx php
 variable "dd_nginx" {
   default = "disabled"
