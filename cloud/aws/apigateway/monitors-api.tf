45d4bee5de7378d241444843dd1c66c56a6b9e22
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2018-02-23 10:31:05 +0100

    MON-48 - Updated with new best practice

--- cloud/aws/apigateway/monitors-api.tf
+++ cloud/aws/apigateway/monitors-api.tf
@@ -1,8 +1,9 @@
 # Monitoring Api Gateway latency
 resource "datadog_monitor" "API_Gateway_latency" {
-  name    = "[${var.environment}] API Gateway latency > ${var.latency_threshold_critical}"
+  name    = "[${var.environment}] API Gateway latency {{comparator}} {{#is_alert}}{{threshold}}%{{/is_alert}}{{#is_warning}}{{warn_threshold}}%{{/is_warning}} ({{value}}%)"
   type    = "metric alert"
   message = "${var.message}"
+
   query = <<EOF
     avg(last_5m): (
       avg:aws.apigateway.latency{${var.filter_tags}} by {region,apiname}
@@ -17,7 +18,7 @@ resource "datadog_monitor" "API_Gateway_latency" {
     critical = "${var.latency_threshold_critical}"
   }
 
-  notify_no_data      = true # Will notify when no data is received
+  notify_no_data      = true  # Will notify when no data is received
   renotify_interval   = 0
   require_full_window = false
   timeout_h           = 0
@@ -28,7 +29,7 @@ resource "datadog_monitor" "API_Gateway_latency" {
 
 # Monitoring API Gateway 5xx errors percent
 resource "datadog_monitor" "API_http_5xx_errors_count" {
-  name    = "[${var.environment}] API Gateway HTTP 5xx errors > ${var.http_5xx_requests_threshold_critical}%"
+  name    = "[${var.environment}] API Gateway HTTP 5xx errors {{comparator}} {{#is_alert}}{{threshold}}%{{/is_alert}}{{#is_warning}}{{warn_threshold}}%{{/is_warning}} ({{value}}%)"
   type    = "metric alert"
   message = "${var.message}"
 
@@ -58,7 +59,7 @@ resource "datadog_monitor" "API_http_5xx_errors_count" {
 
 # Monitoring API Gateway 4xx errors percent
 resource "datadog_monitor" "API_http_4xx_errors_count" {
-  name    = "[${var.environment}] API Gateway HTTP 4xx errors > ${var.http_4xx_requests_threshold_critical}%"
+  name    = "[${var.environment}] API Gateway HTTP 4xx errors {{comparator}} {{#is_alert}}{{threshold}}%{{/is_alert}}{{#is_warning}}{{warn_threshold}}%{{/is_warning}} ({{value}}%)"
   type    = "metric alert"
   message = "${var.message}"
 
