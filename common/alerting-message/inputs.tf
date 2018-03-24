f78bed496c5084c0a7b2969bbb2212c1ad5ab751
Quentin Manfroi <quentin.manfroi@yahoo.fr>
2018-03-24 17:12:22 +0100

    MON-82 Add readme for alerting message module

--- common/alerting-message/inputs.tf
+++ common/alerting-message/inputs.tf
@@ -1,17 +1,21 @@
 variable "oncall_24x7" {
-  type = "string"
+  description = "Define PagerDuty NBH service name for alerts and nodata"
+  type        = "string"
 }
 
 variable "oncall_office_hours" {
-  type = "string"
+  description = "Define PagerDuty BH service name for warning alerts"
+  type        = "string"
 }
 
 variable "prepend_text" {
-  type    = "string"
-  default = ""
+  description = "Optional free text string to prepend to alert"
+  type        = "string"
+  default     = ""
 }
 
 variable "append_text" {
-  type    = "string"
-  default = ""
+  description = "Optional free text string to append to alert"
+  type        = "string"
+  default     = ""
 }
