aea54196f61edf2a1df6ddd15a8f4746dbd51d57
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2018-03-27 11:05:29 +0200

    MON-82 - Nodata variable added and locals used to define it with oncall_alert value

--- common/alerting-message/main.tf
+++ common/alerting-message/main.tf
@@ -1,3 +1,7 @@
+locals {
+  oncall_nodata = "${var.oncall_nodata == "" ? var.oncall_alert : var.oncall_nodata}"
+}
+
 data "template_file" "alerting-message" {
   template = <<EOF
 $${prepend_text}
@@ -5,13 +9,15 @@ $${prepend_text}
 {{#is_recovery}}$${oncall_alert}{{/is_recovery}}
 {{#is_warning}}$${oncall_warning}{{/is_warning}}
 {{#is_warning_recovery}}$${oncall_warning}{{/is_warning_recovery}}
-{{#is_no_data}}$${oncall_alert}{{/is_no_data}}
+{{#is_no_data}}$${oncall_nodata}{{/is_no_data}}
+{{#is_no_data_recovery}}$${oncall_nodata}{{/is_no_data_recovery}}
 $${append_text}
 EOF
 
   vars {
     oncall_alert   = "${var.oncall_alert}"
     oncall_warning = "${var.oncall_warning}"
+    oncall_nodata  = "${local.oncall_nodata}"
     prepend_text   = "${var.prepend_text}"
     append_text    = "${var.append_text}"
   }
