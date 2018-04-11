34ec13c11ac6ab2ccbe3ac9c8dc9e06fde265c81
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2018-04-11 18:39:48 +0200

    MON-168 fix delay variables for php-fpm

--- middleware/php-fpm/monitors-fpm.tf
+++ middleware/php-fpm/monitors-fpm.tf
@@ -26,8 +26,8 @@ resource "datadog_monitor" "datadog_php_fpm_connect_idle" {
   }
 
   notify_no_data      = true
-  evaluation_delay    = "${var.delay_metric}"
-  new_host_delay      = "${var.delay_metric}"
+  evaluation_delay    = "${var.delay}"
+  new_host_delay      = "${var.delay}"
   notify_audit        = false
   timeout_h           = 0
   include_tags        = true
@@ -57,8 +57,8 @@ resource "datadog_monitor" "datadog_fpm_process" {
   }
 
   notify_no_data      = true
-  evaluation_delay    = "${var.delay_service}"
-  new_host_delay      = "${var.delay_service}"
+  evaluation_delay    = "${var.delay}"
+  new_host_delay      = "${var.delay}"
   renotify_interval   = 0
   notify_audit        = false
   timeout_h           = 0
