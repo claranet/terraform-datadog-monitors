6facd04b183922303bcda0a6fdf1a775890d8dc5
Rafael Romero Carmona <rafael.romero.carmona@fr.clara.net>
2018-06-14 18:17:04 +0200

    MON-224 CloudSQL MySQL Network Connections monitor

---
+++ cloud/gcp/cloud-sql/mysql/inputs.tf
@@ -0,0 +1,73 @@
+#
+# Datadog global variables
+#
+variable "environment" {
+  description = "Architecture environment"
+  type        = "string"
+}
+
+variable "filter_tags_use_defaults" {
+  description = "Use default filter tags convention"
+  default     = "true"
+}
+
+variable "filter_tags_custom" {
+  description = "Tags used for custom filtering when filter_tags_use_defaults is false"
+  default     = "*"
+}
+
+variable "message" {
+  description = "Message sent when a monitor is triggered"
+}
+
+variable "delay" {
+  description = "Delay in seconds for the metric evaluation"
+  default     = 900
+}
+
+#
+# Filter variables
+#
+variable "project_id" {
+  type        = "string"
+  description = "ID of the GCP Project"
+}
+
+#
+# Network Connections
+#
+variable "network_connections_message" {
+  description = "Custom message for the Network Connections monitor"
+  type        = "string"
+  default     = ""
+}
+
+variable "network_connections_timeframe" {
+  description = "Timeframe for the Network Connections monitor"
+  type        = "string"
+  default     = "last_5m"
+}
+
+variable "network_connections_hard_limit" {
+  description = "Max number of connections for the CloudSQL Instance. Default value is the max value on https://cloud.google.com/sql/docs/quotas#fixed-limits for MySQL"
+  type        = "string"
+  default     = 4000
+}
+
+variable "network_connections_threshold_warning" {
+  description = "Number of network connections (warning threshold)"
+  type        = "string"
+  default     = 3200
+}
+
+variable "network_connections_threshold_critical" {
+  description = "Number of network connections (critical threshold)"
+  type        = "string"
+  default     = 3600
+}
+
+variable "network_connections_silenced" {
+  description = "Groups to mute for GCP Cloud SQL Network Connections monitor"
+  type        = "map"
+  default     = {}
+}
---
+++ cloud/gcp/cloud-sql/mysql/monitors-cloudsql-mysql.tf
@@ -0,0 +1,54 @@
+#
+# FILTERS
+#
+data "template_file" "filter" {
+  template = "$${filter}"
+
+  vars {
+    filter = "${var.filter_tags_use_defaults == "true" ?
+             format("project_id:%s",var.project_id) :
+             "${var.filter_tags_custom}"}"
+  }
+}
+
+#
+# MySQL Network Connections
+#
+resource "datadog_monitor" "network_connections" {
+  name    = "[${var.environment}] Cloud SQL MySQL Network Connections (hard limit: ${var.network_connections_hard_limit}) {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+  message = "${coalesce(var.network_connections_message, var.message)}"
+
+  type = "metric alert"
+
+  query = <<EOF
+  avg(${var.network_connections_timeframe}):
+    avg:gcp.cloudsql.database.network.connections{${data.template_file.filter.rendered}}
+    by {database_id}
+    > ${var.network_connections_threshold_critical}
+EOF
+
+  thresholds {
+    warning  = "${var.network_connections_threshold_warning}"
+    critical = "${var.network_connections_threshold_critical}"
+  }
+
+  include_tags        = true
+  notify_no_data      = true
+  require_full_window = false
+  renotify_interval   = 0
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  evaluation_delay    = "${var.delay}"
+  new_host_delay      = "${var.delay}"
+  silenced            = "${var.network_connections_silenced}"
+
+  tags = [
+    "team:gcp",
+    "provider:gcp",
+    "env:${var.environment}",
+    "resource:cloud-sql",
+    "engine:mysql",
+  ]
+}
