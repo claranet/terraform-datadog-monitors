d0e2f9106e4aa27006a81f61fa4c346d249aaa16
Rafael Romero Carmona <rafael.romero.carmona@fr.clara.net>
2018-06-15 12:35:55 +0200

    MON-224 Query alert changed to Metric alert

--- cloud/gcp/cloud-sql/instance/monitors-cloud-sql-instance.tf
+++ cloud/gcp/cloud-sql/instance/monitors-cloud-sql-instance.tf
@@ -140,7 +140,7 @@ resource "datadog_monitor" "memory_utilization_forecast" {
   name    = "[${var.environment}] Cloud SQL Memory Utilization Forecast {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
   message = "${coalesce(var.memory_forecast_message, var.message)}"
 
-  type = "query alert"
+  type = "metric alert"
 
   query = <<EOF
   max(${var.memory_forecast_timeframe}):
