e5692365ec734a53b08a4c9ec1380b6525ff0e31
Quentin Manfroi <quentin.manfroi@yahoo.fr>
2018-06-27 18:57:50 +0200

    MON-185 terraform fmt

--- cloud/azure/monitors.tf
+++ cloud/azure/monitors.tf
@@ -220,9 +220,9 @@ module "servicebus" {
   filter_tags_use_defaults = "${var.filter_tags_use_defaults}"
   filter_tags_custom       = "${var.filter_tags_custom}"
 
-  status_silenced                  = "${var.servicebus_status_silenced}"
-  status_message                   = "${var.servicebus_status_message}"
-  status_timeframe                 = "${var.servicebus_status_timeframe}"
+  status_silenced        = "${var.servicebus_status_silenced}"
+  status_message         = "${var.servicebus_status_message}"
+  status_timeframe       = "${var.servicebus_status_timeframe}"
   status_time_aggregator = "${var.servicebus_status_time_aggregator}"
 }
 
