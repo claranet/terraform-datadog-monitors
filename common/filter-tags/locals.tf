faeb0d13bb324278c39e6fe3a7dba77e7983acc6
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2018-08-13 17:17:06 +0200

    MON-273 add optional extra tags support

--- common/filter-tags/locals.tf
+++ common/filter-tags/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  filters = "${var.filter_tags_use_defaults == "true" ? format("dd_monitoring:enabled,dd_%s:enabled,env:%s", var.resource, var.environment) : "${var.filter_tags_custom}"}"
+  filters = "${var.filter_tags_use_defaults == "true" ? join(",", compact(concat(split(",", format("dd_monitoring:enabled,dd_%s:enabled,env:%s", var.resource, var.environment)), compact(var.extra_tags)))) : "${var.filter_tags_custom}"}"
 }
