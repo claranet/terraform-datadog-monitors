d44a0a1a5d4ccb1d98b1c25fd21e1246579d457c
Quentin Manfroi <quentin.manfroi@yahoo.fr>
2018-08-14 15:52:40 +0200

    MON-273 remove module usage where default tagging convention is not available

--- cloud/aws/apigateway/modules.tf
+++
@@ -1,8 +0,0 @@
-module "filter-tags" {
-  source = "../../common/filter-tags"
-
-  environment              = "${var.environment}"
-  resource                 = "apigateway"
-  filter_tags_use_defaults = "${var.filter_tags_use_defaults}"
-  filter_tags_custom       = "${var.filter_tags_custom}"
-}
