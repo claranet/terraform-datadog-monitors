9cccfe2dea4be5eb16848b427c114ea9c4605124
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2018-08-24 18:56:57 +0200

    MON-293 add terraform validate to pipeline

--- scripts/99_terraform.sh
+++ scripts/99_terraform.sh
@@ -4,4 +4,12 @@ set -xueo pipefail
 source "$(dirname $0)/utils.sh"
 goto_root
 
+for path in $(find "$(get_scope $1)" -path ./testing -prune -o -path ./incubator -prune -o -name 'inputs.tf' -print); do
+    cd $(dirname ${path})
+    echo $path
+    terraform init
+    terraform validate --check-variables=false
+    cd -
+done
+
 terraform fmt
