c81cb05668c2090123d6cb07cb7db694e0872b4c
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2018-08-27 13:44:33 +0200

    MON-293 remove useless cd command

--- scripts/99_terraform.sh
+++ scripts/99_terraform.sh
@@ -5,11 +5,9 @@ source "$(dirname $0)/utils.sh"
 goto_root
 
 for path in $(find "$(get_scope $1)" -path ./testing -prune -o -path ./incubator -prune -o -name 'inputs.tf' -print); do
-    cd $(dirname ${path})
-    echo $path
-    terraform init
-    terraform validate --check-variables=false
-    cd -
+    dir=$(dirname ${path})
+    terraform init ${dir}
+    terraform validate --check-variables=false ${dir}
 done
 
 terraform fmt
