73621faac35239b462966caf45be9c1f00b1672a
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2018-08-28 09:43:44 +0200

    MON-237 Fix generated outputs

--- cloud/azure/cosmosdb/outputs.tf
+++ cloud/azure/cosmosdb/outputs.tf
@@ -1,21 +1,21 @@
 output "cosmos_db_status_id" {
   description = "id for monitor cosmos_db_status"
-  value       = "${datadog_monitor.cosmos_db_status.id}"
+  value       = "${datadog_monitor.cosmos_db_status.*.id}"
 }
 
 output "cosmos_db_4xx_requests_id" {
   description = "id for monitor cosmos_db_4xx_requests"
-  value       = "${datadog_monitor.cosmos_db_4xx_requests.id}"
+  value       = "${datadog_monitor.cosmos_db_4xx_requests.*.id}"
 }
 
 output "cosmos_db_5xx_requests_id" {
   description = "id for monitor cosmos_db_5xx_requests"
-  value       = "${datadog_monitor.cosmos_db_5xx_requests.id}"
+  value       = "${datadog_monitor.cosmos_db_5xx_requests.*.id}"
 }
 
 output "cosmos_db_success_no_data_id" {
   description = "id for monitor cosmos_db_success_no_data"
-  value       = "${datadog_monitor.cosmos_db_success_no_data.id}"
+  value       = "${datadog_monitor.cosmos_db_success_no_data.*.id}"
 }
 
 output "cosmos_db_ru_utilization_id" {
