94bceb28bbc15487c4795300bfdc9288913a138f
Quentin Manfroi <quentin.manfroi@yahoo.fr>
2018-08-30 11:33:46 +0200

    MON-224 delete duplicated monitors from mysql

--- cloud/gcp/cloud-sql/mysql/outputs.tf
+++ cloud/gcp/cloud-sql/mysql/outputs.tf
@@ -2,13 +2,3 @@ output "replication_lag_id" {
   description = "id for monitor replication_lag"
   value       = "${datadog_monitor.replication_lag.*.id}"
 }
-
-output "queries_changing_anomaly_id" {
-  description = "id for monitor queries_changing_anomaly"
-  value       = "${datadog_monitor.queries_changing_anomaly.*.id}"
-}
-
-output "questions_changing_anomaly_id" {
-  description = "id for monitor questions_changing_anomaly"
-  value       = "${datadog_monitor.questions_changing_anomaly.*.id}"
-}
