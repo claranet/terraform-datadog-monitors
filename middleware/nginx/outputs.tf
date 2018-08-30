d5ef54845198d6939b06a2f65775723261637494
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2018-08-30 16:14:00 +0200

    MON-289 auto update

--- middleware/nginx/outputs.tf
+++ middleware/nginx/outputs.tf
@@ -5,5 +5,5 @@ output "datadog_nginx_process_id" {
 
 output "datadog_nginx_dropped_connections_id" {
   description = "id for monitor datadog_nginx_dropped_connections"
-  value       = "${datadog_monitor.datadog_nginx_dropped_connections.id}"
+  value       = "${datadog_monitor.datadog_nginx_dropped_connections.*.id}"
 }
