c3ef50e1b900acde209559be2fafe8acca3d51e7
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2018-09-11 13:25:28 +0200

    MON-290 make the status monitor more tolerant

--- middleware/php-fpm/outputs.tf
+++ middleware/php-fpm/outputs.tf
@@ -1,9 +1,9 @@
-output "datadog_php_fpm_connect_idle_id" {
-  description = "id for monitor datadog_php_fpm_connect_idle"
-  value       = "${datadog_monitor.datadog_php_fpm_connect_idle.*.id}"
-}
-
 output "datadog_fpm_process_id" {
   description = "id for monitor datadog_fpm_process"
   value       = "${datadog_monitor.datadog_fpm_process.*.id}"
+}
+
+output "datadog_php_fpm_connect_idle_id" {
+  description = "id for monitor datadog_php_fpm_connect_idle"
+  value       = "${datadog_monitor.datadog_php_fpm_connect_idle.*.id}"
 }
