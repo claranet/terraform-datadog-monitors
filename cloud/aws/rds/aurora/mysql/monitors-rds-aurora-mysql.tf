09e21542f57eeb6b469fd2da42973cb4bc4024e4
Quentin Manfroi <quentin.manfroi@yahoo.fr>
2018-09-14 19:30:39 +0200

    MON-122 add aurora

---
+++ cloud/aws/rds/aurora/mysql/monitors-rds-aurora-mysql.tf
@@ -0,0 +1,34 @@
+### RDS Aurora Mysql Replica Lag monitor ###
+resource "datadog_monitor" "rds_aurora_mysql_replica_lag" {
+  name    = "[${var.environment}] RDS Aurora Mysql replica lag {{#is_alert}}{{{comparator}}} {{threshold}} ms ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}} ms ({{value}}%){{/is_warning}}"
+  message = "${coalesce(var.aurora_replicalag_message, var.message)}"
+
+  type = "metric alert"
+
+  query = <<EOF
+  avg(${var.aurora_replicalag_timeframe}): (
+    avg:aws.rds.aws.rds.aurora_replica_lag{${data.template_file.filter.rendered}} by {region,name}
+  ) > ${var.aurora_replicalag_threshold_critical}
+EOF
+
+  thresholds {
+    warning  = "${var.aurora_replicalag_threshold_warning}"
+    critical = "${var.aurora_replicalag_threshold_critical}"
+  }
+
+  notify_no_data      = true
+  evaluation_delay    = "${var.delay}"
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = false
+  new_host_delay      = "${var.delay}"
+
+  silenced = "${var.aurora_replicalag_silenced}"
+
+  tags = ["env:${var.environment}", "resource:rds", "team:aws", "provider:aws"]
+
+  count = "${var.aurora_cluster_type == "mysql" ? 1 : 0}"
+}
+
