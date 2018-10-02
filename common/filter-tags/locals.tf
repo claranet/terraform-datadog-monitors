937faf728e50054c5f7e9912da8db347d494e24f
Jérôme Respaut <jerome.respaut@fr.clara.net>
2018-10-02 15:39:11 +0200

    MON-301: Refactor a bit terraform code with intermediate locals

--- common/filter-tags/locals.tf
+++ common/filter-tags/locals.tf
@@ -1,4 +1,7 @@
 locals {
-  including = "${var.filter_tags_use_defaults == "true" ? join(",", compact(concat(split(",", format("dd_monitoring:enabled,dd_%s:enabled,env:%s", var.resource, var.environment)), compact(var.extra_tags)))) : "${var.filter_tags_custom}"}"
-  excluding = "${join(",", compact(split(",", "${var.filter_tags_custom_excluded}")))}"
+  including_default_list = "${compact(concat(split(",", format("dd_monitoring:enabled,dd_%s:enabled,env:%s", var.resource, var.environment)), compact(var.extra_tags)))}"
+  excluding_list         = "${compact(split(",", "${var.filter_tags_custom_excluded}"))}"
+
+  including_string = "${var.filter_tags_use_defaults == "true" ? join(",", local.including_default_list) : "${var.filter_tags_custom}"}"
+  excluding_string = "${join(",", local.excluding_list)}"
 }
