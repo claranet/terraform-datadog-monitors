1d542762133738b8326d9f336881ac2922e7871c
Quentin Manfroi <quentin.manfroi@yahoo.fr>
2018-10-02 19:54:03 +0200

    MON-301 ignore excluded tags when filter_tags_use_defaults is true

--- common/filter-tags/locals.tf
+++ common/filter-tags/locals.tf
@@ -1,6 +1,6 @@
 locals {
   including_default_list = "${compact(concat(split(",", format("dd_monitoring:enabled,dd_%s:enabled,env:%s", var.resource, var.environment)), compact(var.extra_tags)))}"
-  excluding_list         = "${compact(split(",", "${var.filter_tags_custom_excluded}"))}"
+  excluding_list         = "${compact(split(",", var.filter_tags_use_defaults == "true" ? "" : join(",", split(",", "${var.filter_tags_custom_excluded}"))))}"
 
   including_string = "${var.filter_tags_use_defaults == "true" ? join(",", local.including_default_list) : "${var.filter_tags_custom}"}"
   excluding_string = "${join(",", local.excluding_list)}"
