e564cf068d3811f828ee96debf99f243521fd461
Jérôme Respaut <jerome.respaut@fr.clara.net>
2018-10-02 15:39:11 +0200

    MON-301: Refactor a bit terraform code with intermediate locals

--- common/filter-tags/outputs.tf
+++ common/filter-tags/outputs.tf
@@ -1,14 +1,14 @@
 output "query_alert" {
   description = "The full filtering pattern including parentheses for service check monitor type"
-  value       = "{${join(",", compact(concat(list(local.including), formatlist("!%s", compact(split(",", local.excluding))))))}}"
+  value       = "{${join(",", compact(concat(list(local.including_string), formatlist("!%s", local.excluding_list))))}}"
 }
 
 output "service_check" {
   description = "The full filtering pattern including braces for query alert monitor type"
-  value       = ".over(\"${replace(local.including, ",", "\",\"")}\")${local.excluding == "" ? "" : ".exclude(\"${replace(local.excluding, ",", "\",\"")}\")"}"
+  value       = ".over(\"${replace(local.including_string, ",", "\",\"")}\")${local.excluding_string == "" ? "" : ".exclude(\"${replace(local.excluding_string, ",", "\",\"")}\")"}"
 }
 
 output "event_alert" {
   description = "The full filtering pattern for event alert monitor type"
-  value       = "tags:${local.including}${local.excluding == "" ? "" : " excluded_tags:${local.excluding}"}"
+  value       = "tags:${local.including_string}${local.excluding_string == "" ? "" : " excluded_tags:${local.excluding_string}"}"
 }
