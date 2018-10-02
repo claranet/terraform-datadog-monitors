9f189ae7f59f432c4fddfa7526cefe755765f094
Quentin Manfroi <quentin.manfroi@yahoo.fr>
2018-10-02 15:52:57 +0200

    MON-301 add excluded tags variable assignation for all modules.tf

--- caas/kubernetes/ingress/modules.tf
+++ caas/kubernetes/ingress/modules.tf
@@ -5,6 +5,7 @@ module "filter-tags" {
   resource                 = "ingress"
   filter_tags_use_defaults = "${var.filter_tags_use_defaults}"
   filter_tags_custom       = "${var.filter_tags_custom}"
+filter_tags_custom_excluded = "${var.filter_tags_custom_excluded}"
   extra_tags               = ["!upstream:upstream-default-backend"]
 }
 
@@ -15,6 +16,7 @@ module "filter-tags-5xx" {
   resource                 = "ingress"
   filter_tags_use_defaults = "${var.filter_tags_use_defaults}"
   filter_tags_custom       = "${var.filter_tags_custom}"
+filter_tags_custom_excluded = "${var.filter_tags_custom_excluded}"
   extra_tags               = ["!upstream:upstream-default-backend,status_code:5xx"]
 }
 
@@ -25,5 +27,6 @@ module "filter-tags-4xx" {
   resource                 = "ingress"
   filter_tags_use_defaults = "${var.filter_tags_use_defaults}"
   filter_tags_custom       = "${var.filter_tags_custom}"
+filter_tags_custom_excluded = "${var.filter_tags_custom_excluded}"
   extra_tags               = ["!upstream:upstream-default-backend,status_code:4xx"]
 }
--- database/mongodb/modules.tf
+++ database/mongodb/modules.tf
@@ -5,6 +5,7 @@ module "filter-tags" {
   resource                 = "mongodb"
   filter_tags_use_defaults = "${var.filter_tags_use_defaults}"
   filter_tags_custom       = "${var.filter_tags_custom}"
+filter_tags_custom_excluded = "${var.filter_tags_custom_excluded}"
 }
 
 module "filter-tags-secondary" {
@@ -14,5 +15,6 @@ module "filter-tags-secondary" {
   resource                 = "mongodb"
   filter_tags_use_defaults = "${var.filter_tags_use_defaults}"
   filter_tags_custom       = "${var.filter_tags_custom}"
+filter_tags_custom_excluded = "${var.filter_tags_custom_excluded}"
   extra_tags               = ["replset_state:secondary"]
 }
