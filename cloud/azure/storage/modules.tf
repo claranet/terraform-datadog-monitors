6b5444c8b8031e8462f89d78169a28a4c5c5cfd6
Alexandre Gaillet <alexandre.gaillet@fr.clara.net>
2018-10-10 15:49:49 +0200

    MON-317 - azure_ added on Azure filter-tags

--- cloud/azure/storage/modules.tf
+++ cloud/azure/storage/modules.tf
@@ -2,7 +2,7 @@ module "filter-tags" {
   source = "../../../common/filter-tags"
 
   environment                 = "${var.environment}"
-  resource                    = "storage"
+  resource                    = "azure_storage"
   filter_tags_use_defaults    = "${var.filter_tags_use_defaults}"
   filter_tags_custom          = "${var.filter_tags_custom}"
   filter_tags_custom_excluded = "${var.filter_tags_custom_excluded}"
