3042f21dafd98a926a8ffc6e9b4f0b254593a0be
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2019-02-04 16:17:15 +0100

    MON-391 Monitors for Azure Search

---
+++ cloud/azure/azure-search/inputs.tf
@@ -0,0 +1,126 @@
+variable "environment" {
+  description = "Architecture environment"
+  type        = "string"
+}
+
+variable "filter_tags_use_defaults" {
+  description = "Use default filter tags convention"
+  default     = "true"
+}
+
+variable "filter_tags_custom" {
+  description = "Tags used for custom filtering when filter_tags_use_defaults is false"
+  default     = "*"
+}
+
+variable "filter_tags_custom_excluded" {
+  description = "Tags excluded for custom filtering when filter_tags_use_defaults is false"
+  default     = ""
+}
+
+variable "message" {
+  description = "Message sent when a monitor is triggered"
+}
+
+variable "evaluation_delay" {
+  description = "Delay in seconds for the metric evaluation"
+  default     = 900
+}
+
+variable "new_host_delay" {
+  description = "Delay in seconds before monitor new resource"
+  default     = 300
+}
+
+# Azure Search specific variables
+variable "latency_silenced" {
+  description = "Groups to mute for Azure Search latency monitor"
+  type        = "map"
+  default     = {}
+}
+
+variable "latency_enabled" {
+  description = "Flag to enable Azure Search latency monitor"
+  type        = "string"
+  default     = "true"
+}
+
+variable "latency_extra_tags" {
+  description = "Extra tags for Azure Search latency monitor"
+  type        = "list"
+  default     = []
+}
+
+variable "latency_message" {
+  description = "Custom message for Azure Search latency monitor"
+  type        = "string"
+  default     = ""
+}
+
+variable "latency_time_aggregator" {
+  description = "Monitor aggregator for Azure Search latency [available values: min, max or avg]"
+  type        = "string"
+  default     = "min"
+}
+
+variable "latency_timeframe" {
+  description = "Monitor timeframe for Azure Search latency [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
+  type        = "string"
+  default     = "last_5m"
+}
+
+variable "latency_threshold_critical" {
+  default     = 4
+  description = "Alerting threshold for Azure Search latency in seconds"
+}
+
+variable "latency_threshold_warning" {
+  default     = 2
+  description = "Warning threshold for Azure Search latency in seconds"
+}
+
+variable "throttled_queries_rate_silenced" {
+  description = "Groups to mute for Azure Search throttled queries rate monitor"
+  type        = "map"
+  default     = {}
+}
+
+variable "throttled_queries_rate_enabled" {
+  description = "Flag to enable Azure Search throttled queries rate monitor"
+  type        = "string"
+  default     = "true"
+}
+
+variable "throttled_queries_rate_extra_tags" {
+  description = "Extra tags for Azure Search throttled queries rate monitor"
+  type        = "list"
+  default     = []
+}
+
+variable "throttled_queries_rate_message" {
+  description = "Custom message for Azure Search throttled queries rate monitor"
+  type        = "string"
+  default     = ""
+}
+
+variable "throttled_queries_rate_time_aggregator" {
+  description = "Monitor aggregator for Azure Search throttled queries rate [available values: min, max or avg]"
+  type        = "string"
+  default     = "min"
+}
+
+variable "throttled_queries_rate_timeframe" {
+  description = "Monitor timeframe for Azure Search throttled queries rate [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
+  type        = "string"
+  default     = "last_5m"
+}
+
+variable "throttled_queries_rate_threshold_critical" {
+  default     = 50
+  description = "Alerting threshold for Azure Search throttled queries rate"
+}
+
+variable "throttled_queries_rate_threshold_warning" {
+  default     = 25
+  description = "Warning threshold for Azure Search throttled queries rate"
+}
---
+++ cloud/azure/azure-search/monitors-azure-search.tf
@@ -0,0 +1,63 @@
+# Monitoring Azure Search latency
+resource "datadog_monitor" "azure_search_latency" {
+  count   = "${var.latency_enabled ? 1 : 0}"
+  name    = "[${var.environment}] Azure Search latency too high {{#is_alert}}{{{comparator}}} {{threshold}}s ({{value}}s){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}s ({{value}}s){{/is_warning}}"
+  type    = "metric alert"
+  message = "${coalesce(var.latency_message, var.message)}"
+
+  query = <<EOF
+    ${var.latency_time_aggregator}(${var.latency_timeframe}): (
+      avg:azure.search_searchservices.search_latency${module.filter-tags.query_alert} by {resource_group,region,name}
+    ) > ${var.latency_threshold_critical}
+  EOF
+
+  evaluation_delay = "${var.evaluation_delay}"
+  new_host_delay   = "${var.new_host_delay}"
+
+  thresholds {
+    warning  = "${var.latency_threshold_warning}"
+    critical = "${var.latency_threshold_critical}"
+  }
+
+  silenced = "${var.latency_silenced}"
+
+  notify_no_data      = true  # Will notify when no data is received
+  renotify_interval   = 0
+  require_full_window = false
+  timeout_h           = 0
+  include_tags        = true
+
+  tags = ["env:${var.environment}", "type:cloud", "provider:azure", "resource:azure-search", "team:claranet", "created-by:terraform", "${var.latency_extra_tags}"]
+}
+
+# Monitoring Azure Search throttled queries
+resource "datadog_monitor" "azure_search_throttled_queries_rate" {
+  count   = "${var.throttled_queries_rate_enabled ? 1 : 0}"
+  name    = "[${var.environment}] Azure Search throttled queries rate is too high {{#is_alert}}{{{comparator}}} {{threshold}}s ({{value}}s){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}s ({{value}}s){{/is_warning}}"
+  type    = "metric alert"
+  message = "${coalesce(var.throttled_queries_rate_message, var.message)}"
+
+  query = <<EOF
+    ${var.throttled_queries_rate_time_aggregator}(${var.throttled_queries_rate_timeframe}): (
+      avg:azure.search_searchservices.throttled_search_queries_percentage${module.filter-tags.query_alert} by {resource_group,region,name}
+    ) > ${var.throttled_queries_rate_threshold_critical}
+  EOF
+
+  evaluation_delay = "${var.evaluation_delay}"
+  new_host_delay   = "${var.new_host_delay}"
+
+  thresholds {
+    warning  = "${var.throttled_queries_rate_threshold_warning}"
+    critical = "${var.throttled_queries_rate_threshold_critical}"
+  }
+
+  silenced = "${var.throttled_queries_rate_silenced}"
+
+  notify_no_data      = true  # Will notify when no data is received
+  renotify_interval   = 0
+  require_full_window = false
+  timeout_h           = 0
+  include_tags        = true
+
+  tags = ["env:${var.environment}", "type:cloud", "provider:azure", "resource:azure-search", "team:claranet", "created-by:terraform", "${var.throttled_queries_rate_extra_tags}"]
+}
