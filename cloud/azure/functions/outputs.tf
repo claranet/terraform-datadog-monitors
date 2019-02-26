7fa04791e75d9cc852c95271b56ed9f7a30c216f
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2019-02-26 11:57:21 +0100

    MON-390 Monitors for Azure Functions

---
+++ cloud/azure/functions/outputs.tf
@@ -0,0 +1,19 @@
+output "function_http_5xx_errors_rate_id" {
+  description = "id for monitor function_http_5xx_errors_rate"
+  value       = "${datadog_monitor.function_http_5xx_errors_rate.*.id}"
+}
+
+output "function_high_connections_count_id" {
+  description = "id for monitor function_high_connections_count"
+  value       = "${datadog_monitor.function_high_connections_count.*.id}"
+}
+
+output "function_high_threads_count_id" {
+  description = "id for monitor function_high_threads_count"
+  value       = "${datadog_monitor.function_high_threads_count.*.id}"
+}
+
+output "function_memory_usage_id" {
+  description = "id for monitor function_memory_usage"
+  value       = "${datadog_monitor.function_memory_usage.*.id}"
+}
