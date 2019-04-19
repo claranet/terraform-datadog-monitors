5e4a37b365f846cca0bbe932f7ca60f18e14448a
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-04-19 20:53:20 +0200

    MON-114 add kubernetes cluster monitors

---
+++ caas/kubernetes/cluster/inputs.tf
@@ -0,0 +1,66 @@
+# Datadog global variables
+
+variable "environment" {
+  description = "Architecture environment"
+}
+
+variable "filter_tags_use_defaults" {
+  description = "Use default filter tags convention"
+  default     = "true"
+}
+
+variable "filter_tags_custom" {
+  description = "Tags used for custom filtering when filter_tags_use_defaults is false"
+  default     = "*"
+}
+
+variable "filter_tags_custom_excluded" {
+  description = "Tags excluded for custom filtering when filter_tags_use_defaults is false"
+  default     = ""
+}
+
+variable "message" {
+  description = "Message sent when a monitor is triggered"
+}
+
+variable "evaluation_delay" {
+  description = "Delay in seconds for the metric evaluation"
+  default     = 15
+}
+
+variable "new_host_delay" {
+  description = "Delay in seconds before monitor new resource"
+  default     = 300
+}
+
+# Datadog monitors variables
+
+variable "apiserver_silenced" {
+  description = "Groups to mute for API server monitor"
+  type        = "map"
+  default     = {}
+}
+
+variable "apiserver_enabled" {
+  description = "Flag to enable API server monitor"
+  type        = "string"
+  default     = "true"
+}
+
+variable "apiserver_extra_tags" {
+  description = "Extra tags for API server monitor"
+  type        = "list"
+  default     = []
+}
+
+variable "apiserver_message" {
+  description = "Custom message for API server monitor"
+  type        = "string"
+  default     = ""
+}
+
+variable "apiserver_threshold_warning" {
+  description = "API server monitor (warning threshold)"
+  type        = "string"
+  default     = 3
+}
---
+++ caas/kubernetes/cluster/monitors-k8s-cluster.tf
@@ -0,0 +1,29 @@
+resource "datadog_monitor" "apiserver" {
+  count   = "${var.apiserver_enabled == "true" ? 1 : 0}"
+  name    = "[${var.environment}] Kubernetes API server does not respond"
+  message = "${coalesce(var.apiserver_message, var.message)}"
+
+  type = "service check"
+
+  query = <<EOQ
+    "kube_apiserver_controlplane.up"${module.filter-tags.service_check}.by("apiserver_name").last(6).count_by_status()
+  EOQ
+
+  thresholds = {
+    warning  = "${var.apiserver_threshold_warning}"
+    critical = 5
+  }
+
+  new_host_delay      = "${var.new_host_delay}"
+  notify_no_data      = true
+  renotify_interval   = 0
+  notify_audit        = false
+  timeout_h           = 0
+  include_tags        = true
+  locked              = false
+  require_full_window = true
+
+  silenced = "${var.apiserver_silenced}"
+
+  tags = ["env:${var.environment}", "type:caas", "provider:kubernetes", "resource:kubernetes-node", "team:claranet", "created-by:terraform", "${var.apiserver_extra_tags}"]
+}
