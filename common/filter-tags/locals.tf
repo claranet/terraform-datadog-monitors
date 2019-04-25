af3266befd28e68c22c32e2b57cf457971943c37
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-04-25 18:52:28 +0200

    MON-441 add extra tags support for custom filtering

--- common/filter-tags/locals.tf
+++ common/filter-tags/locals.tf
@@ -1,7 +1,8 @@
 locals {
   including_default_list = "${compact(concat(split(",", format("dd_monitoring:enabled,dd_%s:enabled,env:%s", var.resource, var.environment)), compact(var.extra_tags)))}"
-  excluding_list         = "${compact(split(",", var.filter_tags_use_defaults == "true" ? "" : join(",", split(",", "${var.filter_tags_custom_excluded}"))))}"
+  including_custom_list  = "${compact(concat(split(",", var.filter_tags_custom), compact(var.extra_tags)))}"
+  excluding_list = "${compact(split(",", var.filter_tags_use_defaults == "true" ? join(",", compact(var.extra_tags_excluded)) : join(",", concat(split(",", var.filter_tags_custom_excluded), compact(var.extra_tags_excluded)))))}"
 
-  including_string = "${var.filter_tags_use_defaults == "true" ? join(",", local.including_default_list) : "${var.filter_tags_custom}"}"
+  including_string = "${var.filter_tags_use_defaults == "true" ? join(",", local.including_default_list) : join(",", local.including_custom_list)}"
   excluding_string = "${join(",", local.excluding_list)}"
 }
