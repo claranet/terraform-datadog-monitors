eb536811446bb251dfb6a880b939fe43c8ef25c0
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-04-25 18:59:17 +0200

    MON-441 update existing monitors using excluded extra tags

--- caas/kubernetes/pod/modules.tf
+++ caas/kubernetes/pod/modules.tf
@@ -16,7 +16,7 @@ module "filter-tags-phase" {
   filter_tags_use_defaults    = "${var.filter_tags_use_defaults}"
   filter_tags_custom          = "${var.filter_tags_custom}"
   filter_tags_custom_excluded = "${var.filter_tags_custom_excluded}"
-  extra_tags                  = ["!phase:pending,!phase:running,!phase:succeeded,!phase:unknown"]
+  extra_tags_excluded         = ["phase:pending,phase:running,phase:succeeded,phase:unknown"]
 }
 
 module "filter-tags-nocrashloopbackoff" {
@@ -27,7 +27,7 @@ module "filter-tags-nocrashloopbackoff" {
   filter_tags_use_defaults    = "${var.filter_tags_use_defaults}"
   filter_tags_custom          = "${var.filter_tags_custom}"
   filter_tags_custom_excluded = "${var.filter_tags_custom_excluded}"
-  extra_tags                  = ["!reason:crashloopbackoff"]
+  extra_tags_excluded         = ["reason:crashloopbackoff"]
 }
 
 module "filter-tags-crashloopbackoff" {
