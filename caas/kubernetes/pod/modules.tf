430a9d8d471854647c07ac9e05a3ee620ab34677
Alexandre Lemaresquier <alexandre.lemaresquier@fr.clara.net>
2019-04-27 15:44:07 +0200

    MON-444 Add filter-tags-nocontainercreating

--- caas/kubernetes/pod/modules.tf
+++ caas/kubernetes/pod/modules.tf
@@ -19,6 +19,17 @@ module "filter-tags-phase" {
   extra_tags_excluded         = ["phase:pending,phase:running,phase:succeeded,phase:unknown"]
 }
 
+module "filter-tags-nocontainercreating" {
+  source = "../../../common/filter-tags"
+
+  environment                 = "${var.environment}"
+  resource                    = "kubernetes"
+  filter_tags_use_defaults    = "${var.filter_tags_use_defaults}"
+  filter_tags_custom          = "${var.filter_tags_custom}"
+  filter_tags_custom_excluded = "${var.filter_tags_custom_excluded}"
+  extra_tags_excluded         = ["reason:containercreating"]
+}
+
 module "filter-tags-nocrashloopbackoff" {
   source = "../../../common/filter-tags"
 
