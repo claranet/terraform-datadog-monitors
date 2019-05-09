b83434c2baf41fc5a4edbda8283459210bdf2c07
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-05-09 09:25:58 +0200

    MON-444 add pod to group by for pod error

--- caas/kubernetes/pod/monitors-k8s-pod.tf
+++ caas/kubernetes/pod/monitors-k8s-pod.tf
@@ -36,7 +36,7 @@ resource "datadog_monitor" "error" {
 
   query = <<EOQ
     ${var.error_time_aggregator}(${var.error_timeframe}):
-      sum:kubernetes_state.container.status_report.count.waiting${module.filter-tags-nocontainercreating.query_alert} by {namespace,reason}.as_count()
+      sum:kubernetes_state.container.status_report.count.waiting${module.filter-tags-nocontainercreating.query_alert} by {namespace,pod,reason}.as_count()
     >= ${var.error_threshold_critical}
   EOQ
 
