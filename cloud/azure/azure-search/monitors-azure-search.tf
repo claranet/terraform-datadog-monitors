da324c10b3aad37c75e2ae920adb8bd34ee9968b
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-06-14 15:12:34 +0200

    MON-476 fmt and add one no data per set as best practice in ci

--- cloud/azure/azure-search/monitors-azure-search.tf
+++ cloud/azure/azure-search/monitors-azure-search.tf
@@ -53,7 +53,7 @@ resource "datadog_monitor" "azure_search_throttled_queries_rate" {
 
   silenced = "${var.throttled_queries_rate_silenced}"
 
-  notify_no_data      = false  # Will notify when no data is received
+  notify_no_data      = false # Will notify when no data is received
   renotify_interval   = 0
   require_full_window = false
   timeout_h           = 0
