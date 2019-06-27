133021c5a75004c351e34b73d5d6039005986a93
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-06-27 12:39:48 +0200

    MON-459 fix extra tags for service bus monitors

--- cloud/gcp/cloud-sql/common/monitors-cloud-sql-common.tf
+++ cloud/gcp/cloud-sql/common/monitors-cloud-sql-common.tf
@@ -25,8 +25,8 @@ EOQ
   timeout_h = 0
   include_tags = true
   require_full_window = false
-  notify_no_data      = false
-  renotify_interval   = 0
+  notify_no_data = false
+  renotify_interval = 0
 
   tags = concat(["env:${var.environment}", "type:cloud", "provider:gcp", "resource:cloud-sql", "team:claranet", "created-by:terraform"], var.cpu_utilization_extra_tags)
 }
@@ -138,8 +138,8 @@ EOQ
   timeout_h = 0
   include_tags = true
   require_full_window = false
-  notify_no_data      = false
-  renotify_interval   = 0
+  notify_no_data = false
+  renotify_interval = 0
 
   tags = concat(["env:${var.environment}", "type:cloud", "provider:gcp", "resource:cloud-sql", "team:claranet", "created-by:terraform"], var.memory_utilization_extra_tags)
 }
