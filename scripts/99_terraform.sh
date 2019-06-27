f904239aa5a0b15fd631bf30bf81bc434d3af117
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-06-27 13:03:48 +0200

    MON-459 update script to delete testing and incubator directories

--- scripts/99_terraform.sh
+++ scripts/99_terraform.sh
@@ -4,7 +4,7 @@ set -xueo pipefail
 source "$(dirname $0)/utils.sh"
 goto_root
 
-for path in $(find "$(get_scope $1)" -path ./testing -prune -o -path ./incubator -prune -o -name 'inputs.tf' -print); do
+for path in $(find "$(get_scope $1)" -name 'inputs.tf' -print); do
     dir=$(dirname ${path})
     terraform init ${dir} >> /dev/null 2>&1
     terraform validate -check-variables=false ${dir}
