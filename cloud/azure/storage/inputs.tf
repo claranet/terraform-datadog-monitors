f96b1d89dda20572fda59f18d23f5b20e197f699
Jérôme Respaut <jerome.respaut@fr.clara.net>
2019-07-19 11:02:36 +0200

    AZ-97: Backport and bump to HCL2/tf0.12

--- cloud/azure/storage/inputs.tf
+++ cloud/azure/storage/inputs.tf
@@ -1,7 +1,7 @@
 # Global Terraform
 variable "environment" {
   description = "Architecture environment"
-  type        = "string"
+  type        = string
 }
 
 # Global DataDog
@@ -42,37 +42,37 @@ variable "filter_tags_custom_excluded" {
 # Azure Storage specific variables
 variable "availability_silenced" {
   description = "Groups to mute for Storage availability monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "availability_enabled" {
   description = "Flag to enable Storage availability monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "availability_extra_tags" {
   description = "Extra tags for Storage availability monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "availability_message" {
   description = "Custom message for Storage availability monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "availability_time_aggregator" {
   description = "Monitor aggregator for Storage availability [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "max"
 }
 
 variable "availability_timeframe" {
   description = "Monitor timeframe for Storage availability [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
@@ -88,37 +88,37 @@ variable "availability_threshold_warning" {
 
 variable "successful_requests_silenced" {
   description = "Groups to mute for Storage sucessful requests monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "successful_requests_enabled" {
   description = "Flag to enable Storage sucessful requests monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "successful_requests_extra_tags" {
   description = "Extra tags for Storage sucessful requests monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "successful_requests_message" {
   description = "Custom message for Storage sucessful requests monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "successful_requests_time_aggregator" {
   description = "Monitor aggregator for Storage sucessful requests [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "max"
 }
 
 variable "successful_requests_timeframe" {
   description = "Monitor timeframe for Storage sucessful requests [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
@@ -134,37 +134,37 @@ variable "successful_storage_requests_threshold_warning" {
 
 variable "latency_silenced" {
   description = "Groups to mute for Storage latency monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "latency_enabled" {
   description = "Flag to enable Storage latency monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "latency_extra_tags" {
   description = "Extra tags for Storage latency monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "latency_message" {
   description = "Custom message for Storage latency monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "latency_time_aggregator" {
   description = "Monitor aggregator for Storage latency [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "min"
 }
 
 variable "latency_timeframe" {
   description = "Monitor timeframe for Storage latency [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
@@ -180,37 +180,37 @@ variable "latency_threshold_warning" {
 
 variable "timeout_error_requests_silenced" {
   description = "Groups to mute for Storage timeout monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "timeout_error_requests_enabled" {
   description = "Flag to enable Storage timeout monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "timeout_error_requests_extra_tags" {
   description = "Extra tags for Storage timeout monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "timeout_error_requests_message" {
   description = "Custom message for Storage timeout monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "timeout_error_requests_time_aggregator" {
   description = "Monitor aggregator for Storage timeout [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "min"
 }
 
 variable "timeout_error_requests_timeframe" {
   description = "Monitor timeframe for Storage timeout [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
@@ -226,37 +226,37 @@ variable "timeout_error_requests_threshold_warning" {
 
 variable "network_error_requests_silenced" {
   description = "Groups to mute for Storage network errors monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "network_error_requests_enabled" {
   description = "Flag to enable Storage network errors monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "network_error_requests_extra_tags" {
   description = "Extra tags for Storage network errors monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "network_error_requests_message" {
   description = "Custom message for Storage network errors monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "network_error_requests_time_aggregator" {
   description = "Monitor aggregator for Storage network errors [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "min"
 }
 
 variable "network_error_requests_timeframe" {
   description = "Monitor timeframe for Storage network errors [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
@@ -272,37 +272,37 @@ variable "network_error_requests_threshold_warning" {
 
 variable "throttling_error_requests_silenced" {
   description = "Groups to mute for Storage throttling error monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "throttling_error_requests_enabled" {
   description = "Flag to enable Storage throttling error monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "throttling_error_requests_extra_tags" {
   description = "Extra tags for Storage throttling error monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "throttling_error_requests_message" {
   description = "Custom message for Storage throttling error monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "throttling_error_requests_time_aggregator" {
   description = "Monitor aggregator for Storage throttling errors [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "min"
 }
 
 variable "throttling_error_requests_timeframe" {
   description = "Monitor timeframe for Storage throttling errors [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
@@ -318,37 +318,37 @@ variable "throttling_error_requests_threshold_warning" {
 
 variable "server_other_error_requests_silenced" {
   description = "Groups to mute for Storage server other errors monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "server_other_error_requests_enabled" {
   description = "Flag to enable Storage server other errors monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "server_other_error_requests_extra_tags" {
   description = "Extra tags for Storage server other errors monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "server_other_error_requests_message" {
   description = "Custom message for Storage server other errors monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "server_other_error_requests_time_aggregator" {
   description = "Monitor aggregator for Storage other errors [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "min"
 }
 
 variable "server_other_error_requests_timeframe" {
   description = "Monitor timeframe for Storage server other errors [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
@@ -364,37 +364,37 @@ variable "server_other_error_requests_threshold_warning" {
 
 variable "client_other_error_requests_silenced" {
   description = "Groups to mute for Storage other errors monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "client_other_error_requests_enabled" {
   description = "Flag to enable Storage other errors monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "client_other_error_requests_extra_tags" {
   description = "Extra tags for Storage other errors monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "client_other_error_requests_message" {
   description = "Custom message for Storage other errors monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "client_other_error_requests_time_aggregator" {
   description = "Monitor aggregator for Storage other errors [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "min"
 }
 
 variable "client_other_error_requests_timeframe" {
   description = "Monitor timeframe for Storage other errors [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
@@ -410,37 +410,37 @@ variable "client_other_error_requests_threshold_warning" {
 
 variable "authorization_error_requests_silenced" {
   description = "Groups to mute for Storage authorization errors monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "authorization_error_requests_enabled" {
   description = "Flag to enable Storage authorization errors monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "authorization_error_requests_extra_tags" {
   description = "Extra tags for Storage authorization errors monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "authorization_error_requests_message" {
   description = "Custom message for Storage authorization errors monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "authorization_error_requests_time_aggregator" {
   description = "Monitor aggregator for Storage authorization errors [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "min"
 }
 
 variable "authorization_error_requests_timeframe" {
   description = "Monitor timeframe for Storage authorization errors [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
@@ -456,36 +456,37 @@ variable "authorization_error_requests_threshold_warning" {
 
 variable "status_time_aggregator" {
   description = "Monitor aggregator for Storage Services status [available values: min, max or avg]"
-  type        = "string"
+  type        = string
   default     = "max"
 }
 
 variable "status_timeframe" {
   description = "Monitor timeframe for Storage Services status [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
-  type        = "string"
+  type        = string
   default     = "last_5m"
 }
 
 variable "status_enabled" {
   description = "Flag to enable App Services status monitor"
-  type        = "string"
+  type        = string
   default     = "true"
 }
 
 variable "status_message" {
   description = "Custom message for storage Services status monitor"
-  type        = "string"
+  type        = string
   default     = ""
 }
 
 variable "status_silenced" {
   description = "Groups to mute for App Services status monitor"
-  type        = "map"
+  type        = map(string)
   default     = {}
 }
 
 variable "status_extra_tags" {
   description = "Extra tags for App Services status monitor"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
+
