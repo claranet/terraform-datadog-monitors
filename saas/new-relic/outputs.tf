31d40303ca5be8376b0cdf9d056589542d3b83f6
Rafael Romero Carmona <rafael.romero.carmona@fr.clara.net>
2019-08-29 12:49:45 +0100

    MON-39 Monitor Data Sent removed. No data is not within the scope of this module.

--- saas/new-relic/outputs.tf
+++ saas/new-relic/outputs.tf
@@ -1,15 +1,10 @@
-output "app_error_rate_id" {
-  description = "id for monitor app_error_rate"
-  value       = datadog_monitor.app_error_rate.*.id
-}
-
 output "app_apdex_score_id" {
   description = "id for monitor app_apdex_score"
   value       = datadog_monitor.app_apdex_score.*.id
 }
 
-output "data_sent_id" {
-  description = "id for monitor data_sent"
-  value       = datadog_monitor.data_sent.*.id
+output "app_error_rate_id" {
+  description = "id for monitor app_error_rate"
+  value       = datadog_monitor.app_error_rate.*.id
 }
 
