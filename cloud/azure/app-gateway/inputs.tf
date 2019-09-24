ba6012e7b1ad731e8d865efbf4e41522ce3c0af4
gauthier.ampe@fr.clara.net <gauthier.ampe@fr.clara.net>
2019-09-24 10:58:32 +0200

    MON-366 Add multiple monitors

--- cloud/azure/app-gateway/inputs.tf
+++ cloud/azure/app-gateway/inputs.tf
@@ -1,3 +1,4 @@
+# Azure App Gateway global variables
 variable "environment" {
   description = "Architecture environment"
   type        = string
@@ -69,11 +70,11 @@ variable "status_timeframe" {
   default     = "last_5m"
 }
 
-# Monitoring App Gateway current_connections (count)
+# Monitoring App Gateway current_connections
 variable "current_connection_enabled" {
   description = "Flag to enable App Gateway current connections monitor"
   type        = string
-  default     = "false"
+  default     = "true"
 }
 
 variable "current_connection_extra_tags" {
@@ -100,11 +101,52 @@ variable "current_connection_timeframe" {
   default     = "last_5m"
 }
 
-# Monitoring App Gateway failed_requests (count)
+# Monitoring App Gateway failed_requests
+variable "appgateway_backend_connect_time_enabled" {
+  description = "Flag to enable App Gateway backend_connect_time monitor"
+  type        = string
+  default     = "true"
+}
+
+variable "appgateway_backend_connect_time_extra_tags" {
+  description = "Extra tags for App Gateway backend_connect_time monitor"
+  type        = list(string)
+  default     = []
+}
+
+variable "appgateway_backend_connect_time_message" {
+  description = "Custom message for App Gateway backend_connect_time monitor"
+  type        = string
+  default     = ""
+}
+
+variable "appgateway_backend_connect_time_time_aggregator" {
+  description = "Monitor aggregator for App Gateway backend_connect_time [available values: min, max or avg]"
+  type        = string
+  default     = "min"
+}
+
+variable "appgateway_backend_connect_time_timeframe" {
+  description = "Monitor timeframe for App Gateway backend_connect_time [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
+  type        = string
+  default     = "last_5m"
+}
+
+variable "appgateway_backend_connect_time_threshold_critical" {
+  default     = 50
+  description = "Maximum critical backend_connect_time errors"
+}
+
+variable "appgateway_backend_connect_time_threshold_warning" {
+  default     = 40
+  description = "Warning regarding backend_connect_time errors"
+}
+
+# Monitoring App Gateway failed_requests
 variable "appgateway_failed_requests_enabled" {
   description = "Flag to enable App Gateway failed requests monitor"
   type        = string
-  default     = "false"
+  default     = "true"
 }
 
 variable "appgateway_failed_requests_extra_tags" {
@@ -132,16 +174,16 @@ variable "appgateway_failed_requests_timeframe" {
 }
 
 variable "appgateway_failed_requests_threshold_critical" {
-  default     = 100
+  default     = 50
   description = "Maximum critical acceptable percent of failed errors"
 }
 
 variable "appgateway_failed_requests_threshold_warning" {
-  default     = 200
+  default     = 40
   description = "Warning regarding acceptable percent of failed errors"
 }
 
-# Monitoring App Gateway healthy_host_count (count)
+# Monitoring App Gateway healthy_host_count
 variable "appgateway_healthy_host_count_enabled" {
   description = "Flag to enable App Gateway healthy host monitor"
   type        = string
@@ -172,7 +214,7 @@ variable "appgateway_healthy_host_count_timeframe" {
   default     = "last_5m"
 }
 
-# Monitoring App Gateway response_status 4xx (count)
+# Monitoring App Gateway response_status 4xx
 variable "appgateway_http_4xx_errors_enabled" {
   description = "Flag to enable App Gateway http 4xx errors monitor"
   type        = string
@@ -213,7 +255,7 @@ variable "appgateway_http_4xx_errors_threshold_warning" {
   description = "Warning regarding acceptable percent of 4xx error"
 }
 
-# Monitoring App Gateway response_status 5xx (count)
+# Monitoring App Gateway response_status 5xx
 variable "appgateway_http_5xx_errors_enabled" {
   description = "Flag to enable App Gateway http 5xx errors monitor"
   type        = string
@@ -250,6 +292,88 @@ variable "appgateway_http_5xx_errors_threshold_critical" {
 }
 
 variable "appgateway_http_5xx_errors_threshold_warning" {
+  default     = 20
+  description = "Warning regarding acceptable percent of 5xx error"
+}
+
+# Monitoring App Gateway Backend response_status 4xx
+variable "appgateway_backend_http_4xx_errors_enabled" {
+  description = "Flag to enable App Gateway http 4xx errors monitor"
+  type        = string
+  default     = "true"
+}
+
+variable "appgateway_backend_http_4xx_errors_extra_tags" {
+  description = "Extra tags for App Gateway http 4xx errors monitor"
+  type        = list(string)
+  default     = []
+}
+
+variable "appgateway_backend_http_4xx_errors_message" {
+  description = "Custom message for App Gateway http 4xx errors monitor"
+  type        = string
+  default     = ""
+}
+
+variable "appgateway_backend_http_4xx_errors_time_aggregator" {
+  description = "Monitor aggregator for App Gateway http 4xx errors [available values: min, max or avg]"
+  type        = string
+  default     = "max"
+}
+
+variable "appgateway_backend_http_4xx_errors_timeframe" {
+  description = "Monitor timeframe for App Gateway http 4xx errors [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
+  type        = string
+  default     = "last_5m"
+}
+
+variable "appgateway_backend_http_4xx_errors_threshold_critical" {
+  default     = 30
+  description = "Minimum critical acceptable percent of 4xx error"
+}
+
+variable "appgateway_backend_http_4xx_errors_threshold_warning" {
+  default     = 20
+  description = "Warning regarding acceptable percent of 4xx error"
+}
+
+# Monitoring App Gateway Backend response_status 5xx
+variable "appgateway_backend_http_5xx_errors_enabled" {
+  description = "Flag to enable App Gateway http 5xx errors monitor"
+  type        = string
+  default     = "true"
+}
+
+variable "appgateway_backend_http_5xx_errors_extra_tags" {
+  description = "Extra tags for App Gateway http 5xx errors monitor"
+  type        = list(string)
+  default     = []
+}
+
+variable "appgateway_backend_http_5xx_errors_message" {
+  description = "Custom message for App Gateway http 5xx errors monitor"
+  type        = string
+  default     = ""
+}
+
+variable "appgateway_backend_http_5xx_errors_time_aggregator" {
+  description = "Monitor aggregator for App Gateway http 5xx errors [available values: min, max or avg]"
+  type        = string
+  default     = "max"
+}
+
+variable "appgateway_backend_http_5xx_errors_timeframe" {
+  description = "Monitor timeframe for App Gateway http 5xx errors [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
+  type        = string
+  default     = "last_5m"
+}
+
+variable "appgateway_backend_http_5xx_errors_threshold_critical" {
+  default     = 30
+  description = "Minimum critical acceptable percent of 5xx error"
+}
+
+variable "appgateway_backend_http_5xx_errors_threshold_warning" {
   default     = 20
   description = "Warning regarding acceptable percent of 5xx error"
 }
