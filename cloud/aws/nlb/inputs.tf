2b2b1371ff6119bde1f522624e75c9c631d44ca0
Jordan Caussat <jordan.caussat@fr.clara.net>
2019-10-18 19:03:21 +0200

    Resolve MON-364 "Monitors for aws nlb"

---
+++ cloud/aws/nlb/inputs.tf
@@ -0,0 +1,73 @@
+# Datadog global variables
+
+variable "environment" {
+  description = "Architecture environment"
+  type        = string
+}
+
+variable "filter_tags_use_defaults" {
+  description = "Use default filter tags convention"
+  default     = "true"
+}
+
+variable "filter_tags_custom" {
+  description = "Tags used for custom filtering when filter_tags_use_defaults is false"
+  default     = "*"
+}
+
+variable "filter_tags_custom_excluded" {
+  description = "Tags excluded for custom filtering when filter_tags_use_defaults is false"
+  default     = ""
+}
+
+variable "message" {
+  description = "Message sent when a monitor is triggered"
+}
+
+variable "evaluation_delay" {
+  description = "Delay in seconds for the metric evaluation"
+  default     = 900
+}
+
+variable "new_host_delay" {
+  description = "Delay in seconds before monitor new resource"
+  default     = 300
+}
+
+variable "prefix_slug" {
+  description = "Prefix string to prepend between brackets on every monitors names"
+  default     = ""
+}
+
+# Datadog monitors variables
+
+variable "nlb_no_healthy_instances_enabled" {
+  description = "Flag to enable NLB no healthy instances monitor"
+  type        = string
+  default     = "true"
+}
+
+variable "nlb_no_healthy_instances_extra_tags" {
+  description = "Extra tags for NLB no healthy instances monitor"
+  type        = list(string)
+  default     = []
+}
+
+variable "nlb_no_healthy_instances_message" {
+  description = "Custom message for NLB no healthy instances monitor"
+  type        = string
+  default     = ""
+}
+
+variable "nlb_no_healthy_instances_time_aggregator" {
+  description = "Monitor aggregator for NLB no healthy instances [available values: min, max or avg]"
+  type        = string
+  default     = "min"
+}
+
+variable "nlb_no_healthy_instances_timeframe" {
+  description = "Monitor timeframe for NLB no healthy instances [available values: `last_#m` (1, 5, 10, 15, or 30), `last_#h` (1, 2, or 4), or `last_1d`]"
+  type        = string
+  default     = "last_5m"
+}
+
