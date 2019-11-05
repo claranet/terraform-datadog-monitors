8cd676114a92d4c180f41fdbbee003c34a742992
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2019-11-05 10:33:27 +0100

    MON-366 Update Azure Application Gateway monitors with suggestions

--- cloud/azure/app-gateway/monitors-app_gateway.tf
+++ cloud/azure/app-gateway/monitors-app_gateway.tf
@@ -31,13 +31,13 @@ EOQ
 # Monitoring App Gateway current_connections
 resource "datadog_monitor" "current_connection" {
   count   = var.current_connection_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway no connection {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway has no connection"
   message = coalesce(var.current_connection_message, var.message)
   type    = "query alert"
 
   query = <<EOQ
     ${var.current_connection_time_aggregator}(${var.current_connection_timeframe}):
-    azure.network_applicationgateways.current_connections${module.filter-tags.query_alert} by {resource_group,name} < 1
+    azure.network_applicationgateways.current_connections${module.filter-tags.query_alert} by {resource_group,region,name} < 1
 EOQ
 
   evaluation_delay    = var.evaluation_delay
@@ -60,13 +60,13 @@ EOQ
 # Monitoring App Gateway backend_connect_time
 resource "datadog_monitor" "appgateway_backend_connect_time" {
   count   = var.appgateway_backend_connect_time_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway backend connect time {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway backend connect time is too high {{#is_alert}}{{{comparator}}} {{threshold}}s ({{value}}s){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}s ({{value}}s){{/is_warning}}"
   message = coalesce(var.appgateway_backend_connect_time_message, var.message)
   type    = "query alert"
 
   query = <<EOQ
     ${var.appgateway_backend_connect_time_time_aggregator}(${var.appgateway_backend_connect_time_timeframe}):
-      sum:azure.network_applicationgateways.backend_connect_time${module.filter-tags.query_alert} by {resource_group,region,name,backendserver} > ${var.appgateway_backend_connect_time_threshold_critical}
+      sum:azure.network_applicationgateways.backend_connect_time${module.filter-tags.query_alert} by {resource_group,region,name,backendhttpsetting,backendpool,backendserver} > ${var.appgateway_backend_connect_time_threshold_critical}
 EOQ
 
   thresholds = {
@@ -100,8 +100,8 @@ resource "datadog_monitor" "appgateway_failed_requests" {
 
   query = <<EOQ
     ${var.appgateway_failed_requests_time_aggregator}(${var.appgateway_failed_requests_timeframe}):
-      default((default(avg:azure.network_applicationgateways.failed_requests${module.filter-tags.query_alert} by {resource_group,region,name}.as_rate(), 0) /
-      default(avg:azure.network_applicationgateways.total_requests${module.filter-tags.query_alert} by {resource_group,region,name}.as_rate(),0)
+      default((default(avg:azure.network_applicationgateways.failed_requests${module.filter-tags.query_alert} by {resource_group,region,name,backendsettingspool}.as_rate(), 0) /
+      default(avg:azure.network_applicationgateways.total_requests${module.filter-tags.query_alert} by {resource_group,region,name,backendsettingspool}.as_rate(),0)
       * 100),0) > ${var.appgateway_failed_requests_threshold_critical}
 EOQ
 
@@ -130,13 +130,13 @@ EOQ
 # Monitoring App Gateway healthy_host_count
 resource "datadog_monitor" "appgateway_healthy_host_count" {
   count   = var.appgateway_healthy_host_count_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway no healthy host {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway backend has no healthy host"
   message = coalesce(var.appgateway_healthy_host_count_message, var.message)
   type    = "query alert"
 
   query = <<EOQ
     ${var.appgateway_healthy_host_count_time_aggregator}(${var.appgateway_healthy_host_count_timeframe}):
-      sum:azure.network_applicationgateways.healthy_host_count${module.filter-tags.query_alert} by {resource_group,region,name} < 1
+      sum:azure.network_applicationgateways.healthy_host_count${module.filter-tags.query_alert} by {resource_group,region,name,backendsettingspool} < 1
 EOQ
 
   evaluation_delay    = var.evaluation_delay
@@ -159,7 +159,7 @@ EOQ
 # Monitoring App Gateway response_status 4xx
 resource "datadog_monitor" "appgateway_http_4xx_errors" {
   count   = var.appgateway_http_4xx_errors_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway HTTP 4xx errors {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway HTTP 4xx errors rate is too high {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
   message = coalesce(var.appgateway_http_4xx_errors_message, var.message)
   type    = "query alert"
 
@@ -194,7 +194,7 @@ EOQ
 # Monitoring App Gateway response_status 5xx
 resource "datadog_monitor" "appgateway_http_5xx_errors" {
   count   = var.appgateway_http_5xx_errors_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway HTTP 5xx errors {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway HTTP 5xx errors rate is too high {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
   message = coalesce(var.appgateway_http_5xx_errors_message, var.message)
   type    = "query alert"
 
@@ -228,14 +228,14 @@ EOQ
 # Monitoring App Gateway Backend response_status 4xx
 resource "datadog_monitor" "appgateway_backend_http_4xx_errors" {
   count   = var.appgateway_backend_http_4xx_errors_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway Backend HTTP 4xx errors {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway backend HTTP 4xx errors rate is too high {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
   message = coalesce(var.appgateway_backend_http_4xx_errors_message, var.message)
   type    = "query alert"
 
   query = <<EOQ
     ${var.appgateway_backend_http_4xx_errors_time_aggregator}(${var.appgateway_backend_http_4xx_errors_timeframe}):
-      default((default(sum:azure.network_applicationgateways.backend_response_status${module.filter-tags-backend-4xx-error.query_alert} by {resource_group,region,name,backendserver}.as_rate(), 0) /
-      default(sum:azure.network_applicationgateways.backend_response_status${module.filter-tags.query_alert} by {resource_group,region,name,backendserver}.as_rate(),0)
+      default((default(sum:azure.network_applicationgateways.backend_response_status${module.filter-tags-backend-4xx-error.query_alert} by {resource_group,region,name,backendhttpsetting,backendpool,backendserver}.as_rate(), 0) /
+      default(sum:azure.network_applicationgateways.backend_response_status${module.filter-tags.query_alert} by {resource_group,region,name,backendhttpsetting,backendpool,backendserver}.as_rate(),0)
       * 100),0) > ${var.appgateway_backend_http_4xx_errors_threshold_critical}
 EOQ
 
@@ -263,14 +263,14 @@ EOQ
 # Monitoring App Gateway Backend response_status 5xx
 resource "datadog_monitor" "appgateway_backend_http_5xx_errors" {
   count   = var.appgateway_backend_http_5xx_errors_enabled == "true" ? 1 : 0
-  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway HTTP Backend 5xx errors {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
+  name    = "${var.prefix_slug == "" ? "" : "[${var.prefix_slug}]"}[${var.environment}] App Gateway backend HTTP 5xx errors rate is too high {{#is_alert}}{{{comparator}}} {{threshold}}% ({{value}}%){{/is_alert}}{{#is_warning}}{{{comparator}}} {{warn_threshold}}% ({{value}}%){{/is_warning}}"
   message = coalesce(var.appgateway_backend_http_5xx_errors_message, var.message)
   type    = "query alert"
 
   query = <<EOQ
     ${var.appgateway_backend_http_5xx_errors_time_aggregator}(${var.appgateway_backend_http_5xx_errors_timeframe}):
-      default((default(sum:azure.network_applicationgateways.backend_response_status${module.filter-tags-backend-5xx-error.query_alert} by {resource_group,region,name,backendserver}.as_rate(), 0) /
-      default(sum:azure.network_applicationgateways.backend_response_status${module.filter-tags.query_alert} by {resource_group,region,name,backendserver}.as_rate(),0)
+      default((default(sum:azure.network_applicationgateways.backend_response_status${module.filter-tags-backend-5xx-error.query_alert} by {resource_group,region,name,backendhttpsetting,backendpool,backendserver}.as_rate(), 0) /
+      default(sum:azure.network_applicationgateways.backend_response_status${module.filter-tags.query_alert} by {resource_group,region,name,backendhttpsetting,backendpool,backendserver}.as_rate(),0)
       * 100),0) > ${var.appgateway_backend_http_5xx_errors_threshold_critical}
 EOQ
 
