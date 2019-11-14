0505c18db5642957484f7fda7acdbc71bad63585
Jean-Maxime LEBLANC <jean-maxime.leblanc@fr.clara.net>
2019-11-14 12:10:16 +0100

    MON-531: Add replicalag time aggregator input variable

--- cloud/aws/rds/aurora/postgresql/monitors-rds-aurora-postgresql.tf
+++ cloud/aws/rds/aurora/postgresql/monitors-rds-aurora-postgresql.tf
@@ -6,7 +6,7 @@ resource "datadog_monitor" "rds_aurora_postgresql_replica_lag" {
   type    = "query alert"
 
   query = <<EOQ
-  avg(${var.aurora_replicalag_timeframe}): (
+  ${var.aurora_replicalag_time_aggregator}(${var.aurora_replicalag_timeframe}): (
     avg:aws.rds.rdsto_aurora_postgre_sqlreplica_lag${module.filter-tags.query_alert} by {region,name}
   ) > ${var.aurora_replicalag_threshold_critical}
 EOQ
