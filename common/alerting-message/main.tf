958553f1d43acfc17956b073b5d90877841bb994
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-12-11 18:18:11 +0100

    MON-543 avoid duplication of same destination

--- common/alerting-message/main.tf
+++ common/alerting-message/main.tf
@@ -4,16 +4,17 @@ $${prepend_text}
 {{#is_alert}} $${message_alert} {{/is_alert}}
 {{#is_warning}} $${message_warning} {{/is_warning}}
 {{#is_no_data}} $${message_nodata} {{/is_no_data}}
-{{#is_recovery}} $${message_alert} $${message_warning} $${message_nodata} {{/is_recovery}}
+{{#is_recovery}} $${message_recovery} {{/is_recovery}}
 $${append_text}
 EOF
 
   vars = {
-    message_alert   = var.message_alert
-    message_warning = var.message_warning
-    message_nodata  = coalesce(var.message_nodata, var.message_alert)
-    prepend_text    = var.prepend_text
-    append_text     = var.append_text
+    message_alert    = var.message_alert
+    message_warning  = coalesce(var.message_warning, var.message_alert)
+    message_nodata   = coalesce(var.message_nodata, var.message_alert)
+    message_recovery = join(" ", compact(distinct(list(var.message_alert, var.message_warning, var.message_nodata))))
+    prepend_text     = var.prepend_text
+    append_text      = var.append_text
   }
 }
 
