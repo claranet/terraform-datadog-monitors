25892d8302efd2077cc401855feba4e236e9866d
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2019-12-13 15:07:14 +0100

    Apply suggestion to common/alerting-message/main.tf

--- common/alerting-message/main.tf
+++ common/alerting-message/main.tf
@@ -11,7 +11,7 @@ EOF
   vars = {
     message_alert    = var.message_alert
     message_warning  = var.message_warning == "false" ? var.message_alert : var.message_warning
-    message_nodata   = var.message_nodata == "false" ? var.message_alert : var.message_nodata
+    message_nodata   = var.message_nodata == null ? var.message_alert : var.message_nodata
     message_recovery = join(" ", compact(distinct(list(var.message_alert, var.message_warning, var.message_nodata))))
     prepend_text     = var.prepend_text
     append_text      = var.append_text
