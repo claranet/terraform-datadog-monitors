ebfa547cff4f034ec6ab1badd6a3a21f36db3715
Pierre-Yves Gillier <pierre-yves.gillier@fr.clara.net>
2020-01-17 11:00:35 +0100

    MON-521 add fargate monitors

---
+++ cloud/aws/ecs/fargate/modules.tf
@@ -0,0 +1,14 @@
+module "filter-tags" {
+
+  source = "../../../../common/filter-tags"
+
+  environment = var.environment
+  resource    = "aws_ecs"
+  extra_tags_excluded = [
+    "ecs_container_name:datadog-agent",
+    "ecs_container_name:_internal_ecs_pause"
+  ]
+  filter_tags_use_defaults    = var.filter_tags_use_defaults
+  filter_tags_custom          = var.filter_tags_custom
+  filter_tags_custom_excluded = var.filter_tags_custom_excluded
+}
