bbf5a213c0f0315b627a6573d9721bbb9ab4955f
Nicolas Senaud <nicolas.senaud@fr.clara.net>
2020-01-22 18:55:54 +0100

    Add configurable no_data_timeframe to cloud/aws/elb

--- cloud/aws/elb/inputs.tf
+++ cloud/aws/elb/inputs.tf
@@ -25,6 +25,12 @@ variable "notify_no_data" {
   default     = true
 }
 
+variable "elb_no_healthy_instance_no_data_timeframe" {
+  description = "Number of minutes before reporting no data"
+  type        = string
+  default     = 10
+}
+
 variable "message" {
   description = "Message sent when an alert is triggered"
 }
