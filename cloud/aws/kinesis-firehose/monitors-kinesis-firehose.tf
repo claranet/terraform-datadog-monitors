0f27e1681082cd529fd68f0fa8f4accbfe2889f1
Nicolas Senaud <nicolas.senaud@fr.clara.net>
2020-01-22 18:57:51 +0100

    Add configurable no_data_timeframe to cloud/aws/kinesis-firehose

--- cloud/aws/kinesis-firehose/monitors-kinesis-firehose.tf
+++ cloud/aws/kinesis-firehose/monitors-kinesis-firehose.tf
@@ -18,6 +18,7 @@ EOQ
   evaluation_delay    = var.evaluation_delay
   new_host_delay      = var.new_host_delay
   notify_no_data      = var.notify_no_data
+  no_data_timeframe   = var.firehose_incoming_records_no_data_timeframe
   renotify_interval   = 0
   notify_audit        = false
   timeout_h           = 0
