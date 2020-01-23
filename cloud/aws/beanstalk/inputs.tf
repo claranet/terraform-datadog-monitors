ccb321d244c3cede806c2718a3c98173d455a458
Guillaume Dondero <guillaume.dondero@fr.clara.net>
2020-01-23 11:38:14 +0100

    MON-556: correct default value for health_no_data_timeframe

--- cloud/aws/beanstalk/inputs.tf
+++ cloud/aws/beanstalk/inputs.tf
@@ -28,7 +28,7 @@ variable "notify_no_data" {
 variable "health_no_data_timeframe" {
   description = "Number of minutes before reporting no data"
   type        = string
-  default     = 10
+  default     = 20
 }
 
 variable "message" {
