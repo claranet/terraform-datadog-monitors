62d28f1163dc04a64c74bd5d4c32e97b9f920d9d
Guillaume Dondero <guillaume.dondero@fr.clara.net>
2020-01-23 11:41:31 +0100

    MON-556: correct default value for firehose_incoming_records_no_data_timeframe

--- cloud/aws/kinesis-firehose/inputs.tf
+++ cloud/aws/kinesis-firehose/inputs.tf
@@ -28,7 +28,7 @@ variable "notify_no_data" {
 variable "firehose_incoming_records_no_data_timeframe" {
   description = "Number of minutes before reporting no data"
   type        = string
-  default     = 10
+  default     = 30
 }
 
 variable "message" {
