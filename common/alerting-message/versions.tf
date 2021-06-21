7763abb6739e70ed02aba9b7f9a9c12e807217fe
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2021-06-21 19:46:49 +0200

    add datadog constraint version to 3.1.0 minimum

--- common/alerting-message/versions.tf
+++ common/alerting-message/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     template = {
       source = "hashicorp/template"
+      version = ">= 3.1.0"
     }
   }
   required_version = ">= 0.12.26"
--- network/tls/versions.tf
+++ network/tls/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     datadog = {
       source = "terraform-providers/datadog"
+      version = ">= 3.1.0"
     }
   }
   required_version = ">= 0.12.26"
