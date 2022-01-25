d8a6717b279b0d96229f0a7a2a7edd72f2ca3f4c
Tom Kaplonski <tomasz.kaplonski@sellics.com>
2022-01-25 09:17:02 +0100

    Recovery message goes to the same group as entering warning/alert/nodata status

--- common/alerting-message/main.tf
+++ common/alerting-message/main.tf
@@ -2,9 +2,14 @@ data "template_file" "alerting-message" {
   template = <<EOF
 $${prepend_text}
 {{#is_alert}} $${message_alert} {{/is_alert}}
+{{#is_alert_recovery}} $${message_alert}} {{/is_alert_recovery}}
+{{#is_alert_to_warning}} $${message_alert} $${message_warning} {{/is_alert_to_warning}}
+
 {{#is_warning}} $${message_warning} {{/is_warning}}
+{{#is_warning_recovery}} $${message_warning} {{/is_warning_recovery}}
+
 {{#is_no_data}} $${message_nodata} {{/is_no_data}}
-{{#is_recovery}} $${message_recovery} {{/is_recovery}}
+{{#is_no_data_recovery}} $${message_nodata} {{/is_no_data_recovery}}
 $${append_text}
 EOF
 
@@ -12,7 +17,6 @@ EOF
     message_alert    = var.message_alert
     message_warning  = var.message_warning == null ? var.message_alert : var.message_warning
     message_nodata   = var.message_nodata == null ? var.message_alert : var.message_nodata
-    message_recovery = join(" ", compact(distinct([var.message_alert, var.message_warning, var.message_nodata])))
     prepend_text     = var.prepend_text
     append_text      = var.append_text
   }
