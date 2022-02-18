f789f99088ecc8f10d6ef00124be0c7c9ec0a383
adrien.rodenburg <adrien.rodenburg@fr.clara.net>
2022-02-18 17:28:01 +0100

    Added CosmosDB

--- cloud/azure/cosmosdb/modules.tf
+++ cloud/azure/cosmosdb/modules.tf
@@ -8,6 +8,17 @@ module "filter-tags" {
   filter_tags_custom_excluded = var.filter_tags_custom_excluded
 }
 
+module "filter-tags-status" {
+  source = "../../../common/filter-tags"
+
+  environment                 = var.environment
+  resource                    = "azure_cosmosdb"
+  filter_tags_use_defaults    = var.filter_tags_use_defaults
+  filter_tags_custom          = var.filter_tags_custom
+  filter_tags_custom_excluded = var.filter_tags_custom_excluded
+  extra_tags                  = ["status:running"]
+}
+
 module "filter-tags-statuscode" {
   source = "../../../common/filter-tags"
 
