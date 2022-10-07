31ad1d7e1e2d2759c987d4a3f72e34371e842f38
adrien.rodenburg <adrien.rodenburg@fr.clara.net>
2022-10-07 14:32:45 +0200

    [SOXINT-7023] Added tag for status monitors

--- cloud/azure/app-services/monitors-app_services.tf
+++ cloud/azure/app-services/monitors-app_services.tf
@@ -161,7 +161,7 @@ resource "datadog_monitor" "appservices_status" {
   message = coalesce(var.status_message, var.message)
 
   query = <<EOQ
-      ${var.status_time_aggregator}(${var.status_timeframe}):avg:azure.app_services.count${module.filter-tags.query_alert} by {resource_group,region,name} < 1
+      ${var.status_time_aggregator}(${var.status_timeframe}):avg:azure.app_services.count${module.filter-tags-status.query_alert} by {resource_group,region,name} < 1
 EOQ
 
   monitor_thresholds {
