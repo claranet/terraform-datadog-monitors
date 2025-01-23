6e660386bd8141c5acbc14b4acda1ac3b4399391
Matthieu Bourgain <matthieu.bourgain@claranet.com>
2025-01-23 10:10:28 +0100

    Add nginx is down monitor

--- caas/kubernetes/ingress/vts/outputs.tf
+++ caas/kubernetes/ingress/vts/outputs.tf
@@ -1,3 +1,8 @@
+output "nginx_ingress_is_down_id" {
+  description = "id for monitor nginx_ingress_is_down"
+  value       = datadog_monitor.nginx_ingress_is_down.*.id
+}
+
 output "nginx_ingress_too_many_4xx_id" {
   description = "id for monitor nginx_ingress_too_many_4xx"
   value       = datadog_monitor.nginx_ingress_too_many_4xx.*.id
